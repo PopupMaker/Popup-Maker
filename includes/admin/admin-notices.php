8f8d8817be0d466c57f29867135f4085f846d799
Daniel Iser <danieliser@wizardinternetsolutions.com>
2015-02-20 00:38:35 -0500

    Fixed unnecessary calls to session_start.

--- includes/admin/admin-notices.php
+++ includes/admin/admin-notices.php
@@ -1,17 +1,19 @@
 <?php class PopMake_Admin_Notice {
 	protected static $messages = array();
-	public static function factory() {
+	public static function init() {
 		if ( !session_id() )
 			session_start();
 		//add_action('admin_notices',array('PopMake_Admin_Notice','render_notices'));
 	}
 	public static function get_messages($type = NULL) {
+		self::init();
 		if(empty($_SESSION[POPMAKE_SLUG.'_notices']))
 			return array();
 		$messages = $_SESSION[POPMAKE_SLUG.'_notices'];
 		return $messages;
 	}
 	public static function render_notices() {
+		self::init();
 		foreach(PopMake_Admin_Notice::get_messages() as $key => $message) {
 			?><div class="<?php esc_html_e($message['type']);?>">
 				<p><?php esc_html_e($message['message']);?></p>
@@ -20,6 +22,7 @@
 		}
 	}
 	public static function add($message, $type = 'updated') {
+		self::init();
 		if ( !session_id() ) session_start();
 		$_SESSION[POPMAKE_SLUG.'_notices'][] = array(
 			'message' => $message,
@@ -27,4 +30,3 @@
 		);
 	}
 }
-PopMake_Admin_Notice::factory();
