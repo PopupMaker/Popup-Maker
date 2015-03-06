e47934915f17ce76e8f328ee7b4456fb499b8a57
Daniel Iser <danieliser@wizardinternetsolutions.com>
2015-03-05 23:46:06 -0500

    Changed popup preview content to allow both echo and get methods.

--- includes/admin/themes/metabox-preview.php
+++ includes/admin/themes/metabox-preview.php
@@ -1,7 +1,10 @@
 <?php
 
 add_action('popmake_example_popup_content', 'popmake_default_example_popup_content',1);
-function popmake_default_example_popup_content()
-{?>
-	<p>Suspendisse ipsum eros, tincidunt sed commodo ut, viverra vitae ipsum. Etiam non porta neque. Pellentesque nulla elit, aliquam in ullamcorper at, bibendum sed eros. Morbi non sapien tellus, ac vestibulum eros. In hac habitasse platea dictumst. Nulla vestibulum, diam vel porttitor placerat, eros tortor ultrices lectus, eget faucibus arcu justo eget massa. Maecenas id tellus vitae justo posuere hendrerit aliquet ut dolor.</p>
-<?php }
+function popmake_default_example_popup_content() {
+	echo popmake_get_default_example_popup_content();
+}
+
+function popmake_get_default_example_popup_content() {
+	return '<p>Suspendisse ipsum eros, tincidunt sed commodo ut, viverra vitae ipsum. Etiam non porta neque. Pellentesque nulla elit, aliquam in ullamcorper at, bibendum sed eros. Morbi non sapien tellus, ac vestibulum eros. In hac habitasse platea dictumst. Nulla vestibulum, diam vel porttitor placerat, eros tortor ultrices lectus, eget faucibus arcu justo eget massa. Maecenas id tellus vitae justo posuere hendrerit aliquet ut dolor.</p>';
+}
