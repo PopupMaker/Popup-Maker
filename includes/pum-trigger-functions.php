b938fe8e9101b0a9b95e3899e9c8eac71049557a
Daniel Iser <danieliser@wizardinternetsolutions.com>
2015-11-11 01:11:27 -0500

    Updated arguments for core trigger registration.

--- includes/pum-trigger-functions.php
+++ includes/pum-trigger-functions.php
@@ -1,5 +1,10 @@
 <?php
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 /**
  * Gets all cookie triggers.
  *
@@ -71,12 +76,59 @@ function pum_trigger_cookie_fields( $custom_fields = array() ) {
 	), $custom_fields ) );
 }
 
+/**
+ * Returns an array of args for registering triggers.
+ *
+ * @uses filter pum_get_triggers
+ *
+ * @return array
+ */
 function pum_get_triggers() {
-	return apply_filters( 'pum_triggers', array(
+	return apply_filters( 'pum_get_triggers', array(
+		'click_open' => array(
+			'id' => 'click_open',
+			'labels' => array(
+				'name' => __( 'Click Open', 'popup-maker' ),
+				'modal_title' => __( 'Click Trigger Settings', 'popup-maker' ),
+				'settings_column' => sprintf(
+					'<strong>%1$s</strong>: %2$s',
+					__( 'Extra Selectors', 'popup-maker' ),
+					'<%= extra_selectors %>'
+				),
+			),
+			'<strong>Extra Selectors</strong>: <%= extra_selectors %>',
+			'sections' => array(
+				'general' => array(
+					'title' => __( 'General', 'popup-maker' ),
+				),
+			),
+			'fields' => array(
+				'general' => array(
+					'extra_selectors'          => array(
+						'label'       => __( 'Extra CSS Selectors', 'popup-maker' ),
+						'desc'        => __( 'This allows custom css classes, ids or selector strings to trigger the popup when clicked. Separate multiple selectors using commas.', 'popup-maker' ),
+						'placeholder' => __( '.my-class, #button2', 'popup-maker' ),
+						'priority'    => 1,
+					),
+				),
+			),
+		),
 		'auto_open' => array(
 			'id' => 'auto_open',
 			'labels' => array(
+				'name' => __( 'Auto Open', 'popup-maker' ),
 				'modal_title' => __( 'Auto Open Settings', 'popup-maker' ),
+				'settings_column' => sprintf(
+					'<strong>%1$s</strong>: %2$s <strong>%3$s</strong>: %4$s',
+					__( 'Delay', 'popup-maker' ),
+					'<%= delay %>',
+					__( 'Cookie', 'popup-maker' ),
+					sprintf( '%s%s%s',
+						'<%= I10n.labels.cookie_triggers[cookie.trigger] %><% if (cookie.trigger !== "disabled") { %> / <% if (typeof cookie.session === "undefined") { %><%= cookie.time %><% } else { %>',
+						__( 'Sessions', 'popup-maker' ),
+						'<% } %><% } %>'
+					)
+				),
 			),
 			'sections' => array(
 				'general' => array(
@@ -100,33 +152,16 @@ function pum_get_triggers() {
 				'cookie' => pum_trigger_cookie_fields(),
 			)
 		),
-		'click_open' => array(
-			'id' => 'click_open',
-			'labels' => array(
-				'modal_title' => __( 'Click Trigger Settings', 'popup-maker' ),
-			),
-			'sections' => array(
-				'general' => array(
-					'title' => __( 'General', 'popup-maker' ),
-				),
-			),
-			'fields' => array(
-				'general' => array(
-					'extra_selectors'          => array(
-						'label'       => __( 'Extra CSS Selectors', 'popup-maker' ),
-						'desc'        => __( 'This allows custom css classes, ids or selector strings to trigger the popup when clicked. Separate multiple selectors using commas.', 'popup-maker' ),
-						'placeholder' => __( '.my-class, #button2', 'popup-maker' ),
-						'priority'    => 1,
-					),
-				),
-			)
-		)
 	) );
 }
 
+/**
+ * Registers triggers on the WP `init` action.
+ *
+ * @uses function pum_get_triggers
+ */
 function pum_register_triggers() {
 	$triggers = pum_get_triggers();
-
 	PUM_Triggers::instance()->add_triggers( $triggers );
 }
 add_action( 'init', 'pum_register_triggers' );
