c025cb2cbc5e8bd3a1cb2c2ee47c6fdba4a2457a
danieliser <danieliser@wizardinternetsolutions.com>
2015-12-16 03:49:43 -0500

    Added version check so that only one set of filters will be applied. This is necessary since popup content runs through both filters for backward compatibility.

--- includes/pum-template-hooks.php
+++ includes/pum-template-hooks.php
@@ -1,31 +1,37 @@
 <?php
 
-
-add_filter( 'pum_popup_content', array( $GLOBALS['wp_embed'], 'run_shortcode' ), 8 );
-add_filter( 'pum_popup_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
-add_filter( 'pum_popup_content', 'wptexturize', 10 );
-add_filter( 'pum_popup_content', 'convert_smilies', 10 );
-add_filter( 'pum_popup_content', 'convert_chars', 10 );
-add_filter( 'pum_popup_content', 'wpautop', 10 );
-add_filter( 'pum_popup_content', 'shortcode_unautop', 10 );
-add_filter( 'pum_popup_content', 'prepend_attachment', 10 );
-add_filter( 'pum_popup_content', 'force_balance_tags', 10 );
-add_filter( 'pum_popup_content', 'do_shortcode', 11 );
-add_filter( 'pum_popup_content', 'capital_P_dangit', 11 );
-add_filter( 'pum_popup_content', 'popmake_popup_content_container', 10000, 2 );
-
+/**
+ * @since 1.4.0 hooks & filters
+ */
+if ( version_compare( POPMAKE_VERSION, '1.4.0', '>=' ) ) {
+	add_filter( 'pum_popup_content', array( $GLOBALS['wp_embed'], 'run_shortcode' ), 8 );
+	add_filter( 'pum_popup_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
+	add_filter( 'pum_popup_content', 'wptexturize', 10 );
+	add_filter( 'pum_popup_content', 'convert_smilies', 10 );
+	add_filter( 'pum_popup_content', 'convert_chars', 10 );
+	add_filter( 'pum_popup_content', 'wpautop', 10 );
+	add_filter( 'pum_popup_content', 'shortcode_unautop', 10 );
+	add_filter( 'pum_popup_content', 'prepend_attachment', 10 );
+	add_filter( 'pum_popup_content', 'force_balance_tags', 10 );
+	add_filter( 'pum_popup_content', 'do_shortcode', 11 );
+	add_filter( 'pum_popup_content', 'capital_P_dangit', 11 );
+	add_filter( 'pum_popup_content', 'popmake_popup_content_container', 10000, 2 );
+}
 /**
  * @deprecated 1.4.0 hooks & filters
  */
-add_filter( 'the_popup_content', array( $GLOBALS['wp_embed'], 'run_shortcode' ), 8 );
-add_filter( 'the_popup_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
-add_filter( 'the_popup_content', 'wptexturize', 10 );
-add_filter( 'the_popup_content', 'convert_smilies', 10 );
-add_filter( 'the_popup_content', 'convert_chars', 10 );
-add_filter( 'the_popup_content', 'wpautop', 10 );
-add_filter( 'the_popup_content', 'shortcode_unautop', 10 );
-add_filter( 'the_popup_content', 'prepend_attachment', 10 );
-add_filter( 'the_popup_content', 'force_balance_tags', 10 );
-add_filter( 'the_popup_content', 'do_shortcode', 11 );
-add_filter( 'the_popup_content', 'capital_P_dangit', 11 );
-add_filter( 'the_popup_content', 'popmake_popup_content_container', 10000, 2 );
+else {
+	add_filter( 'the_popup_content', array( $GLOBALS['wp_embed'], 'run_shortcode' ), 8 );
+	add_filter( 'the_popup_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
+	add_filter( 'the_popup_content', 'wptexturize', 10 );
+	add_filter( 'the_popup_content', 'convert_smilies', 10 );
+	add_filter( 'the_popup_content', 'convert_chars', 10 );
+	add_filter( 'the_popup_content', 'wpautop', 10 );
+	add_filter( 'the_popup_content', 'shortcode_unautop', 10 );
+	add_filter( 'the_popup_content', 'prepend_attachment', 10 );
+	add_filter( 'the_popup_content', 'force_balance_tags', 10 );
+	add_filter( 'the_popup_content', 'do_shortcode', 11 );
+	add_filter( 'the_popup_content', 'capital_P_dangit', 11 );
+	add_filter( 'the_popup_content', 'popmake_popup_content_container', 10000, 2 );
+}
+
