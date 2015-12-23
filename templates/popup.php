13f41b34792a9000a63971976c75a4e8667b0c6c
danieliser <danieliser@wizardinternetsolutions.com>
2015-12-22 20:43:47 -0500

    Reformatted layout html & prefixes as well as implemented pum_popup_classes for all elements.

--- templates/popup.php
+++ templates/popup.php
@@ -1,13 +1,51 @@
-<div id="popmake-<?php popmake_the_popup_ID(); ?>" class="<?php popmake_the_popup_classes(); ?>" <?php popmake_the_popup_data_attr(); ?>>
+<div id="pum-<?php pum_popup_ID(); ?>" class="<?php pum_popup_classes(); ?>" <?php pum_popup_data_attr(); ?>>
 
-	<?php do_action('popmake_popup_before_inner'); ?>
+	<div id="popmake-<?php pum_popup_ID(); ?>" class="<?php pum_popup_classes( null, 'container' ); ?>">
 
-	<?php if( popmake_get_the_popup_title() != '' ) : ?>
-	<div class="popmake-title"><?php popmake_the_popup_title(); ?></div>
-	<?php endif; ?>
+		<?php do_action( 'pum_popup_before_title' ); ?>
+		<?php do_action( 'popmake_popup_before_inner' ); // Backward compatibility. ?>
 
-	<?php popmake_the_popup_content( ); ?>
 
-	<?php do_action('popmake_popup_after_inner'); ?>
+		<?php
+		/**
+		 * Render the title if not empty.
+		 */
+		?>
+		<?php if ( pum_get_popup_title() != '' ) : ?>
+			<div class="<?php pum_popup_classes( null, 'title' ); ?>">
+				<?php pum_popup_title(); ?>
+			</div>
+		<?php endif; ?>
+
+
+		<?php do_action( 'pum_popup_before_content' ); ?>
+
+
+		<?php
+		/**
+		 * Render the content.
+		 */
+		?>
+		<div class="<?php pum_popup_classes( null, 'content' ); ?>">
+			<?php pum_popup_content(); ?>
+		</div>
+
+
+		<?php do_action( 'pum_popup_after_content' ); ?>
+		<?php do_action( 'popmake_popup_after_inner' ); // Backward compatibility. ?>
+
+
+		<?php
+		/**
+		 * Render the close button if needed.
+		 */
+		?>
+		<?php if ( pum_show_close_button() ) : ?>
+			<span class="<?php pum_popup_classes( null, 'close' ); ?>">
+			<?php pum_popup_close_text(); ?>
+		</span>
+		<?php endif; ?>
+
+	</div>
 
 </div>
