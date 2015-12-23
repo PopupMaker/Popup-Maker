0feb7a7e00912827b3a8c6b8ac68ceb90ef6bd61
danieliser <danieliser@wizardinternetsolutions.com>
2015-12-22 20:45:15 -0500

    Added pum_get_popup_title, pum_popup_ID, pum_popup_close_text & pum_show_close_button functions.

--- includes/pum-popup-functions.php
+++ includes/pum-popup-functions.php
@@ -1,5 +1,10 @@
 <?php
 
+function pum_get_popup_title( $popup_id = null ) {
+	$popup = new PUM_Popup( $popup_id );
+	return $popup->get_title();
+}
+
 
 function pum_get_popup_triggers( $popup_id = null ) {
 	if ( ! ( $popup = new PUM_Popup( $popup_id ) ) ) {
