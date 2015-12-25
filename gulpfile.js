87451711a76b88b0f75bd33a55e16604193078b7
danieliser <danieliser@wizardinternetsolutions.com>
2015-12-25 03:41:14 -0500

    Rebuilt package.json with proper formatting and reduced dependency packages.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,21 @@ gulp.task('sass', function() {
         .pipe($.csscomb())
         .pipe(gulp.dest('assets/css'));
 });
-gulp.task('css', function() {
+
+
+gulp.task('css', ['sass'], function() {
+    return gulp.src(['!assets/css/*.min.css', 'assets/css/*.css', ])
+        // Minify the CSS
+        .pipe($.csso())
+        // Rename the file with the .min.css extension
+        .pipe($.rename({ extname: '.min.css' }))
+        // Save the file
+        .pipe(gulp.dest('assets/css'));
+        //.pipe($.livereload());
+
+});
+
+gulp.task('css:minify', function() {
     return gulp.src(['!assets/css/*.min.css', 'assets/css/*.css', ])
         // Minify the CSS
         .pipe($.csso())
@@ -21,9 +35,17 @@ gulp.task('css', function() {
         .pipe($.rename({ extname: '.min.css' }))
 
         // Save the file
-        .pipe(gulp.dest('assets/css'))
-        .pipe($.livereload());
+        .pipe(gulp.dest('assets/css'));
 });
+
+gulp.task('css:lint', function() {
+    return gulp.src(['!assets/css/*.min.css', 'assets/css/*.css', ])
+        // Lint the CSS
+        .pipe($.csslint())
+        .pipe($.csslint.reporter());
+});
+
+gulp.task('js', ['js:admin', 'js:site', 'js:other']);
 
 gulp.task('js:admin', function() {
     return gulp.src(['assets/js/src/admin/plugins/**/*.js', 'assets/js/src/admin/general.js'])
@@ -31,8 +53,8 @@ gulp.task('js:admin', function() {
         .pipe(gulp.dest('assets/js'))
         .pipe($.uglify())
         .pipe($.rename({extname: '.min.js'}))
-        .pipe(gulp.dest('assets/js'))
-        .pipe($.livereload());
+        .pipe(gulp.dest('assets/js'));
+        //.pipe($.livereload());
 });
 gulp.task('js:site', function() {
     return gulp.src(['assets/js/src/site/plugins/**/*.js', 'assets/js/src/site/general.js'])
@@ -46,16 +68,16 @@ gulp.task('js:site', function() {
         .pipe(gulp.dest('assets/js'))
         .pipe($.uglify())
         .pipe($.rename({extname: '.min.js'}))
-        .pipe(gulp.dest('assets/js'))
-        .pipe($.livereload());
+        .pipe(gulp.dest('assets/js'));
+        //.pipe($.livereload());
 });
 gulp.task('js:other', function() {
     return gulp.src('assets/js/src/*.js')
         .pipe(gulp.dest('assets/js'))
         .pipe($.uglify())
         .pipe($.rename({extname: '.min.js'}))
-        .pipe(gulp.dest('assets/js'))
-        .pipe($.livereload());
+        .pipe(gulp.dest('assets/js'));
+        //.pipe($.livereload());
 });
 
 
@@ -84,3 +106,7 @@ gulp.task('watch', function() {
     gulp.watch('assets/js/src/site/**/*.js', ['js:site']);
     gulp.watch(['assets/js/src/**/*.js', '!assets/js/src/site/**/*.js', '!assets/js/src/admin/**/*.js'], ['js:other']);
 });
+
+gulp.task('build', ['css', 'js']);
+
+gulp.task('default', ['build']);
