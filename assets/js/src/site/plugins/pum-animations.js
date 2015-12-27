a8a803b02e264ca418a297c74d4b9169c5c2b24c
danieliser <danieliser@wizardinternetsolutions.com>
2015-12-27 03:33:48 -0500

    Updated animations to use PUM.getPopup and to properly work with new Popup HTML Structures.

--- assets/js/src/site/plugins/pum-animations.js
+++ assets/js/src/site/plugins/pum-animations.js
@@ -7,20 +7,25 @@
 
     $.fn.popmake.methods.animate_overlay = function (style, duration, callback) {
         // Method calling logic
-        var $this = $(this),
-            settings = $this.data('popmake');
+        var settings = PUM.getPopup(this).popmake('getSettings');
+
         if (settings.meta.display.overlay_disabled) {
-            callback();
+            // Fire user passed callback.
+            if (callback !== undefined) {
+                callback();
+                // TODO Test this new method. Then remove the above.
+                //callback.apply(this);
+            }
         } else {
             if ($.fn.popmake.overlay_animations[style]) {
-                return $.fn.popmake.overlay_animations[style].apply(this, Array.prototype.slice.call(arguments, 1));
+                return $.fn.popmake.overlay_animations[style].apply(this, [duration, callback]);
             }
             $.error('Animation style ' + $.fn.popmake.overlay_animations + ' does not exist.');
         }
         return this;
     };
 
-    $.fn.popmake.methods.animate = function (style, callback) {
+    $.fn.popmake.methods.animate = function (style) {
         // Method calling logic
         if ($.fn.popmake.animations[style]) {
             return $.fn.popmake.animations[style].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -31,83 +36,88 @@
 
     $.fn.popmake.animations = {
         none: function (callback) {
-            var $this = $(this);
-            $this.popmake('animate_overlay', 'none', 0, function () {
-                $this.css({display: 'block'});
-                if (callback !== undefined) {
-                    callback();
-                }
-            });
+            PUM.getPopup(this)
+                .popmake('animate_overlay', 'none', null, function () {
+                    // Fire user passed callback.
+                    if (callback !== undefined) {
+                        callback();
+                        // TODO Test this new method. Then remove the above.
+                        //callback.apply(this);
+                    }
+                });
             return this;
         },
         slide: function (callback) {
-            var $this = $(this).show(0).css({opacity: 0}),
-                settings = $this.data('popmake'),
+            var $popup = PUM.getPopup(this).show(0).css({opacity: 0}),
+                $container = $popup.popmake('getContainer').show(0).css({opacity: 0}),
+                settings = $popup.popmake('getSettings'),
                 speed = settings.meta.display.animation_speed / 2,
-                start = $this.popmake('animation_origin', settings.meta.display.animation_origin);
+                start = $popup.popmake('animation_origin', settings.meta.display.animation_origin);
 
-            if (!settings.meta.display.position_fixed && !$.fn.isScrolling()) {
-                $('html').css('overflow-x', 'hidden');
-            }
-
-            $this
+            $container
                 .position(start)
+                .css({opacity: 1});
+
+            $popup
                 .css({opacity: 1})
                 .popmake('animate_overlay', 'fade', speed, function () {
-                    $this.popmake('reposition', function (position) {
-
-                        $this.animate(position, speed, 'swing', function () {
-                            if (!settings.meta.display.position_fixed) {
-                                $('html').css('overflow-x', 'inherit');
-                            }
+                    $container.popmake('reposition', function (position) {
+                        $container.animate(position, speed, 'swing', function () {
+                            // Fire user passed callback.
                             if (callback !== undefined) {
                                 callback();
+                                // TODO Test this new method. Then remove the above.
+                                //callback.apply(this);
                             }
                         });
-
                     });
                 });
             return this;
         },
         fade: function (callback) {
-            var $this = $(this).show(0).css({opacity: 0}),
-                settings = $this.data('popmake'),
+            var $popup = PUM.getPopup(this),
+                $container = $popup.popmake('getContainer'),
+                settings = $popup.popmake('getSettings'),
                 speed = settings.meta.display.animation_speed / 2;
 
-            $this
-                .popmake('animate_overlay', 'fade', speed, function () {
+            $container
+                .show(0)
+                .css({opacity: 0});
 
-                    $this.animate({opacity: 1}, speed, 'swing', function () {
-                        if (callback !== undefined) {
-                            callback();
-                        }
-                    });
-
+            $popup.popmake('animate_overlay', 'fade', speed, function () {
+                $container.animate({opacity: 1}, speed, 'swing', function () {
+                    // Fire user passed callback.
+                    if (callback !== undefined) {
+                        callback();
+                        // TODO Test this new method. Then remove the above.
+                        //callback.apply(this);
+                    }
                 });
+            });
             return this;
         },
         fadeAndSlide: function (callback) {
-            var $this = $(this).show(0).css({opacity: 0}),
-                settings = $this.data('popmake'),
+            var $popup = PUM.getPopup(this).show(0).css({opacity: 0}),
+                $container = $popup.popmake('getContainer').show(0).css({opacity: 0}),
+                settings = $popup.popmake('getSettings'),
                 speed = settings.meta.display.animation_speed / 2,
-                start = $this.popmake('animation_origin', settings.meta.display.animation_origin);
+                start = $popup.popmake('animation_origin', settings.meta.display.animation_origin);
 
-            if (!settings.meta.display.position_fixed && !$.fn.isScrolling()) {
-                $('html').css('overflow-x', 'hidden');
-            }
+            $container.position(start);
 
-            $this
-                .position(start)
+            $popup
+                .hide()
+                .css({opacity: 1})
                 .popmake('animate_overlay', 'fade', speed, function () {
-                    $this.popmake('reposition', function (position) {
+                    $container.popmake('reposition', function (position) {
 
                         position.opacity = 1;
-                        $this.animate(position, speed, 'swing', function () {
-                            if (!settings.meta.display.position_fixed) {
-                                $('html').css('overflow-x', 'inherit');
-                            }
+                        $container.animate(position, speed, 'swing', function () {
+                            // Fire user passed callback.
                             if (callback !== undefined) {
                                 callback();
+                                // TODO Test this new method. Then remove the above.
+                                //callback.apply(this);
                             }
                         });
 
@@ -115,147 +125,32 @@
                 });
             return this;
         },
-        grow: function (callback) {
-            /*            var $this = $(this).show(0).css({ opacity: 0 }),
-             settings = $this.data('popmake'),
-             speed = settings.meta.display.animation_speed / 2,
-             origin = settings.meta.display.animation_origin,
-             original_size = {height: $this.height(), width: $this.width()};
-
-             if (origin === 'top' || origin === 'bottom') {
-             origin = 'center ' + origin;
-             }
-             if (origin === 'left' || origin === 'right') {
-             origin = origin + ' center';
-             }
-
-             $this.css({
-             opacity: 1
-             });
-
-             $this.popmake('animate_overlay', 'fade', speed, function () {
-             // Reposition with callback. position returns default positioning.
-             $this.popmake('reposition', function (position) {
-
-             position.height = original_size.height;
-             position.width = original_size.width;
-             $this.css({
-             height: 0,
-             width: 0
-             }).animate(position, speed, 'swing', function () {
-             if (callback !== undefined) {
-             callback();
-             }
-             });
-
-             });
-             });
-             return this;
-             */
-            var $this = $(this).show(0).css({opacity: 0}),
-                settings = $this.data('popmake'),
-                speed = settings.meta.display.animation_speed / 2,
-                start = $this.popmake('animation_origin', settings.meta.display.animation_origin);
-
-            if (!settings.meta.display.position_fixed && !$.fn.isScrolling()) {
-                $('html').css('overflow-x', 'hidden');
-            }
-
-            $this
-                .position(start)
-                .css({opacity: 1})
-                .popmake('animate_overlay', 'fade', speed, function () {
-                    $this.popmake('reposition', function (position) {
-
-                        $this.animate(position, speed, 'swing', function () {
-                            if (!settings.meta.display.position_fixed) {
-                                $('html').css('overflow-x', 'inherit');
-                            }
-                            if (callback !== undefined) {
-                                callback();
-                            }
-                        });
-
-                    });
-                });
-            return this;
-
+        /**
+         * TODO: Remove these and let import script replace them.
+         * @deprecated
+         * @returns {$.fn.popmake.animations}
+         */
+        grow: function () {
+            return $.fn.popmake.animations.fade.apply(this, Array.prototype.slice.call(arguments, 1));
         },
-        growAndSlide: function (callback) {
-            var $this = $(this).show(0).css({opacity: 0}),
-                settings = $this.data('popmake'),
-                speed = settings.meta.display.animation_speed / 2,
-                start = $this.popmake('animation_origin', settings.meta.display.animation_origin);
-
-            if (!settings.meta.display.position_fixed && !$.fn.isScrolling()) {
-                $('html').css('overflow-x', 'hidden');
-            }
-
-            $this
-                .position(start)
-                .css({opacity: 1})
-                .popmake('animate_overlay', 'fade', speed, function () {
-                    $this.popmake('reposition', function (position) {
-
-                        $this.animate(position, speed, 'swing', function () {
-                            if (!settings.meta.display.position_fixed) {
-                                $('html').css('overflow-x', 'inherit');
-                            }
-                            if (callback !== undefined) {
-                                callback();
-                            }
-                        });
-
-                    });
-                });
-            return this;
-            /*
-             var $this = $(this).show(0).css({ opacity: 0 }),
-             settings = $this.data('popmake'),
-             speed = settings.meta.display.animation_speed / 2000,
-             origin = settings.meta.display.animation_origin,
-             start = $this.popmake('animation_origin', origin);
-
-             if (!settings.meta.display.position_fixed && !$.fn.isScrolling()) {
-             $('html').css('overflow-x', 'hidden');
-             }
-
-             $this.position(start);
-
-             TweenLite.to($this, 0, { scale: 0, opacity: 1, transformOrigin: '0 0' });
-
-             $this.popmake('animate_overlay', 'fade', speed * 1000, function () {
-             $this.popmake('reposition', function (position) {
-
-             TweenLite.to($this, speed, $.extend(position, {
-             scale: 1,
-             transformOrigin: '50% 50%',
-             onComplete: function () {
-             if (!settings.meta.display.position_fixed) {
-             $('html').css('overflow-x', 'inherit');
-             }
-             if (callback !== undefined) {
-             callback();
-             }
-             }
-             }));
-
-             });
-             });
-             return this;
-             */
+        /**
+         * @deprecated
+         * @returns {$.fn.popmake.animations}
+         */
+        growAndSlide: function () {
+            return $.fn.popmake.animations.fadeAndSlide.apply(this, Array.prototype.slice.call(arguments, 1));
         }
     };
 
     $.fn.popmake.overlay_animations = {
         none: function (duration, callback) {
-            $('#popmake-overlay').show(duration, callback);
+            PUM.getPopup(this).show(duration, callback);
         },
         fade: function (duration, callback) {
-            $('#popmake-overlay').fadeIn(duration, callback);
+            PUM.getPopup(this).fadeIn(duration, callback);
         },
         slide: function (duration, callback) {
-            $('#popmake-overlay').slideDown(duration, callback);
+            PUM.getPopup(this).slideDown(duration, callback);
         }
     };
 
