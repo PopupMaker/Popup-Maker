4ef8e7bf61e0d76ba334a8f950f2022a9db6d69f
Daniel Iser <danieliser@wizardinternetsolutions.com>
2016-03-16 03:17:52 -0400

    Added a check for missing function and included it if needed.

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -18,6 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 function popmake_get_default_popup_theme() {
 	$default_theme = get_option( 'popmake_default_theme' );
 	if ( false === get_post_status( $default_theme ) ) {
+		if ( ! function_exists( 'popmake_install_default_theme' ) ) {
+			include_once POPMAKE_DIR . 'includes/install.php';
+		}
 		popmake_install_default_theme();
 		$default_theme = get_option( 'popmake_default_theme' );
 		pum_force_theme_css_refresh();
