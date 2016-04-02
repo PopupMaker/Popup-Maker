42a0ad8056ea8b1369e148f81d501276651ad059
Daniel Iser <danieliser@wizardinternetsolutions.com>
2016-04-02 17:41:33 -0400

    Removed throttling of the theme updates to make them more fluid.

--- assets/js/src/admin/plugins/colorpickers.js
+++ assets/js/src/admin/plugins/colorpickers.js
@@ -11,20 +11,30 @@ var PUMColorPickers;
                         if ($input.hasClass('background-color')) {
                             $input.parents('table').find('.background-opacity').show();
                         }
-                        PUMUtils.throttle(function () {
-                            PopMakeAdmin.update_theme();
-                        }, 50);
+
+                        $(this).trigger('change.update');
+
+                        PopMakeAdmin.update_theme();
                     },
                     clear: function (e) {
                         var $input = $(e.currentTarget).prev();
                         if ($input.hasClass('background-color')) {
                             $input.parents('table').find('.background-opacity').hide();
                         }
+
+                        $(this).prev('input').trigger('change.clear').wpColorPicker('close');
+
                         PopMakeAdmin.update_theme();
                     }
                 });
         }
     };
 
-    $(document).on('pum_init', PUMColorPickers.init);
+    $(document)
+        .on('click', '.iris-palette', function () {
+            console.log(this);
+            $(this).parents('.wp-picker-active').find('input.color-picker').trigger('change');
+            setTimeout(PopMakeAdmin.update_theme, 500);
+        })
+        .on('pum_init', PUMColorPickers.init);
 }(jQuery, document));
