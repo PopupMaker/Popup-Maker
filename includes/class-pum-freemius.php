9d5b0c2a2d6eff5d302a9638740ea7d1d0013a69
Daniel Iser <danieliser@wizardinternetsolutions.com>
2016-09-29 19:34:38 -0400

    Removed comment.

--- includes/class-pum-freemius.php
+++ includes/class-pum-freemius.php
@@ -91,14 +91,13 @@ class PUM_Freemius {
 	 * Renders the popup maker usage statistics permission notification.
 	 */
 	public function permission_list( $permissions = array() ) {
-		/*
         $permissions['metrics'] = array(
             'icon-class'    => 'dashicons dashicons-performance',
             'label'         => __( 'Usage Statistics', 'popup-maker' ),
             'desc'          => __( 'Popup & Theme Counts, Open Counts', 'popup-maker' ),
             'priority'      => 25,
         );
-		*/
+
         return $permissions;
 	}
 
