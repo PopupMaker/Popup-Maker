fa987c2dc897b458cba3ecb51eade40f2557027e
Daniel Iser <danieliser@wizardinternetsolutions.com>
2016-09-29 20:47:17 -0400

    Fixed bug where color didn't update properly when first clicked in theme editor. #344

--- assets/js/src/admin/plugins/colorpickers.js
+++ assets/js/src/admin/plugins/colorpickers.js
@@ -6,30 +6,11 @@ var PUMColorPickers;
             $('.color-picker').filter(':not(.initialized)')
                 .addClass('initialized')
                 .wpColorPicker({
-                    change: function (e) {
-                        var $this = $(this),
-                            $input = $(e.currentTarget);
-                        if ($input.hasClass('background-color')) {
-                            $input.parents('table').find('.background-opacity').show();
-                        }
-
-                        $this.trigger('change.update');
-
-                        if ($('form#post input#post_type').val() === 'popup_theme') {
-                            PopMakeAdmin.update_theme();
-                        }
+                    change: function (event, ui) {
+                        $(event.target).trigger('colorchange', ui);
                     },
-                    clear: function (e) {
-                        var $input = $(e.currentTarget).prev();
-                        if ($input.hasClass('background-color')) {
-                            $input.parents('table').find('.background-opacity').hide();
-                        }
-
-                        $(this).prev('input').trigger('change.clear').wpColorPicker('close');
-
-                        if ($('form#post input#post_type').val() === 'popup_theme') {
-                            PopMakeAdmin.update_theme();
-                        }
+                    clear: function (event) {
+                        $(event.target).prev().trigger('colorchange').wpColorPicker('close');
                     }
                 });
         }
@@ -39,6 +20,29 @@ var PUMColorPickers;
         .on('click', '.iris-palette', function () {
             $(this).parents('.wp-picker-active').find('input.color-picker').trigger('change');
             setTimeout(PopMakeAdmin.update_theme, 500);
+        })
+        .on('colorchange', function (event, ui) {
+            var $input = $(event.target),
+                $opacity = $input.parents('tr').next('tr.background-opacity'),
+                color = '';
+
+            if (ui !== undefined && ui.color !== undefined) {
+                color = ui.color.toString();
+            }
+
+            if ($input.hasClass('background-color')) {
+                if (typeof color === 'string' && color.length) {
+                    $opacity.show();
+                } else {
+                    $opacity.hide();
+                }
+            }
+
+            $input.val(color);
+
+            if ($('form#post input#post_type').val() === 'popup_theme') {
+                PopMakeAdmin.update_theme();
+            }
         })
         .on('pum_init', PUMColorPickers.init);
 }(jQuery, document));
