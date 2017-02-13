455e2a288d244c922ef3e17c0c267c2a84526998
Daniel Iser <danieliser@wizardinternetsolutions.com>
2017-02-12 22:36:59 -0500

    Remove Priority support references #381

--- includes/admin/metabox-support.php
+++ includes/admin/metabox-support.php
@@ -42,12 +42,6 @@ function popmake_support_meta_box_links() { ?>
 				<span><?php _e( 'Extension Support', 'popup-maker' ); ?></span>
 			</a>
 		</li>
-		<li>
-			<a href="https://wppopupmaker.com/support/priority-pricing?utm_source=Plugin+Admin&utm_medium=Support+Metabox&utm_campaign=Priority+Support">
-				<img src="<?php echo POPMAKE_URL; ?>/assets/images/admin/member-forums.png"/>
-				<span><?php _e( 'Priority Support', 'popup-maker' ); ?></span>
-			</a>
-		</li>
 	</ul>
 	<?php
 }
