1e63aec370ac8b0dda21b61b1b258f0ec66aa95b
Daniel Iser <danieliser@wizardinternetsolutions.com>
2017-07-15 16:53:22 -0400

    Sanitized tab key.

--- includes/admin/metabox-support.php
+++ includes/admin/metabox-support.php
@@ -27,28 +27,30 @@ function popmake_support_meta_box_links() {
 
 	$source = $pagenow;
 
+	$active_tab = isset( $_GET['tab'] ) && array_key_exists( $_GET['tab'], popmake_get_settings_tabs() ) ? $_GET['tab'] : null;
+
 	if ( isset( $_GET['page'] ) && $_GET['page'] == 'pum-settings' ) {
-		$source = 'plugin-settings-page' . ( !empty( $_GET['tab'] ) ? '-' . $_GET['tab'] . '-tab' : '');
+		$source = 'plugin-settings-page' . ( ! empty( $active_tab ) ? '-' . $active_tab . '-tab' : '' );
 	} elseif ( isset( $_GET['page'] ) && $_GET['page'] == 'pum-tools' ) {
-		$source = 'plugin-tools-page' . ( !empty( $_GET['tab'] ) ? '-' . $_GET['tab'] . '-tab' : '');
+		$source = 'plugin-tools-page' . ( ! empty( $active_tab ) ? '-' . $active_tab . '-tab' : '' );
 	}
 	?>
 	<ul class="popmake-support-links">
 		<li>
-            <a href="http://docs.wppopupmaker.com/?utm_medium=support-sidebar&utm_campaign=ContextualHelp&utm_source=<?php echo $source; ?>&utm_content=documentation">
-				<img src="<?php echo POPMAKE_URL; ?>/assets/images/support-pane-docs-icon.png"/>
-                <span><?php _e( 'Documentation', 'popup-maker' ); ?></span>
+			<a href="http://docs.wppopupmaker.com/?utm_medium=support-sidebar&utm_campaign=ContextualHelp&utm_source=<?php echo $source; ?>&utm_content=documentation">
+				<img src="<?php echo POPMAKE_URL; ?>/assets/images/support-pane-docs-icon.png" />
+				<span><?php _e( 'Documentation', 'popup-maker' ); ?></span>
 			</a>
 		</li>
 		<li>
 			<a href="https://wordpress.org/support/plugin/popup-maker">
-				<img src="<?php echo POPMAKE_URL; ?>/assets/images/support-pane-wpforums-icon.png"/>
+				<img src="<?php echo POPMAKE_URL; ?>/assets/images/support-pane-wpforums-icon.png" />
 				<span><?php _e( 'Free Support Forums', 'popup-maker' ); ?></span>
 			</a>
 		</li>
 		<li>
 			<a href="https://wppopupmaker.com/support/?utm_medium=support-sidebar&utm_campaign=ContextualHelp&utm_source=<?php echo $source; ?>&utm_content=extension-support">
-				<img src="<?php echo POPMAKE_URL; ?>/assets/images/support-pane-extensions-icon.png"/>
+				<img src="<?php echo POPMAKE_URL; ?>/assets/images/support-pane-extensions-icon.png" />
 				<span><?php _e( 'Extension Support', 'popup-maker' ); ?></span>
 			</a>
 		</li>
