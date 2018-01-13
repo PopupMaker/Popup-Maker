65b54fe5dabdd319d5bfc5c38d55bbadf00709d9
Daniel Iser <danieliser@wizardinternetsolutions.com>
2018-01-13 00:04:09 -0500

    Added new interface for Post Type data migrations.

---
+++ classes/Interface/Upgrade/Posts.php
@@ -0,0 +1,25 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2018, WP Popup Maker
+ ******************************************************************************/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Second-level interface for registering a batch process that leverages
+ * pre-fetch and data storage.
+ *
+ * @since  1.7.0
+ */
+interface PUM_Interface_Upgrade_Posts extends PUM_Interface_Batch_PrefetchProcess {
+
+	/**
+	 * Used to filter popup query based on conditional info.
+	 *
+	 * @return array Returns an array of popup post type query args for this upgrade.
+	 */
+	public function custom_query_args();
+
+}
