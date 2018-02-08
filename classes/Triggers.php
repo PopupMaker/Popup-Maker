c080d04c75c3cc5b9a710a1fbb9c7cef5823d55c
Daniel Iser <danieliser@wizardinternetsolutions.com>
2018-02-08 04:54:27 -0500

    Text tweak #475

--- classes/Triggers.php
+++ classes/Triggers.php
@@ -142,9 +142,9 @@ class PUM_Triggers {
 					'general'  => array(
 						'extra_selectors' => array(
 							'label'       => __( 'Extra CSS Selectors', 'popup-maker' ),
+							'desc'        => __( 'For more than one selector, separate by comma (,)', 'popup-maker' ) . '<br /><strong>eg:  </strong>' . __( ' .class-here, .class-2-here, #button_id', 'popup-maker' ),
 							'placeholder' => __( '.class-here', 'popup-maker' ),
-							'desc'        => __( 'For more than one selector, separate by comma (,)', 'popup-maker' ) . '<br /><strong>eg:  </strong>' . __( ' .class-here, .class-2-here, #button', 'popup-maker' ),
-							'doclink'     => 'http://docs.wppopupmaker.com/article/147-getting-css-selectors?utm_source=plugin-page-popup-editor=&utm_medium=inline-doclink&utm_campaign=ContextualHelp&utm_content=extra-selectors',
+							'doclink'     => 'http://docs.wppopupmaker.com/article/147-getting-css-selectors?utm_source=plugin-popup-editor=&utm_medium=inline-doclink&utm_campaign=ContextualHelp&utm_content=extra-selectors',
 						),
 					),
 					'advanced' => array(
