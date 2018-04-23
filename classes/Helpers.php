4a73b457bbc99c959c4aff33eb8a0c973e502650
Daniel Iser <danieliser@wizardinternetsolutions.com>
2018-04-23 03:50:59 -0400

    Added helper function to return array of shortcodes and data in usable format from any content.

--- classes/Helpers.php
+++ classes/Helpers.php
@@ -26,6 +26,32 @@ class PUM_Helpers {
 		return $content;
 	}
 
+	public static function get_shortcodes_from_content( $content ) {
+		$pattern = get_shortcode_regex();
+		$shortcodes = array();
+		if ( preg_match_all( '/' . $pattern . '/s', $content, $matches ) ) {
+			foreach ( $matches[0] as $key => $value ) {
+				$shortcodes[$key] = array(
+					'full_text' => $value,
+					'tag' => $matches[2][$key],
+					'atts' => shortcode_parse_atts( $matches[3][$key] ),
+					'content' => $matches[5][$key],
+				);
+
+				if ( ! empty( $shortcodes[$key]['atts'] ) ) {
+					foreach ( $shortcodes[$key]['atts'] as $attr_name => $attr_value ) {
+						// Filter numeric keys as they are valueless/truthy attributes.
+						if ( is_numeric( $attr_name ) ) {
+							$shortcodes[$key]['atts'][$attr_value] = true;
+							unset( $shortcodes[$key]['atts'] );
+						}
+					}
+				}
+			}
+		}
+
+		return $shortcodes;
+	}
 	public static function upload_dir_url( $path = '' ) {
 		$upload_dir = wp_upload_dir();
 		$upload_dir = $upload_dir['baseurl'];
