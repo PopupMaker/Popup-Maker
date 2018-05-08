b68e744e205fac416e75792dc377380b6b870489
Daniel Iser <danieliser@wizardinternetsolutions.com>
2018-05-08 04:39:42 -0400

    Added class for GDPR / WP Privacy support.

---
+++ classes/Privacy.php
@@ -0,0 +1,18 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2018, WP Popup Maker
+ ******************************************************************************/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+/**
+ * Class PUM_Privacy
+ */
+class PUM_Privacy {
+
+	public static function init() {
+	}
+}
