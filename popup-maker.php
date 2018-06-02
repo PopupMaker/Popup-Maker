4ac46523fc405a8ab5adeafda2f0d7ad9ec634ec
Daniel Iser <danieliser@wizardinternetsolutions.com>
2018-06-02 03:24:12 -0400

    Added sanity check for specific class property.

--- popup-maker.php
+++ popup-maker.php
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function pum_autoloader( $class ) {
 
-	if ( strncmp( 'PUM_Newsletter_', $class, strlen( 'PUM_Newsletter_' ) ) === 0 && class_exists( 'PUM_MCI' ) && version_compare( PUM_MCI::$VER, '1.3.0', '<' ) ) {
+	if ( strncmp( 'PUM_Newsletter_', $class, strlen( 'PUM_Newsletter_' ) ) === 0 && class_exists( 'PUM_MCI' ) && ! empty( PUM_MCI::$VER ) && version_compare( PUM_MCI::$VER, '1.3.0', '<' ) ) {
 		return;
 	}
 
@@ -93,7 +93,7 @@ class Popup_Maker {
 	/**
 	 * @var string Plugin Version
 	 */
-	public static $VER = '1.7.22';
+	public static $VER = '1.7.23';
 
 	/**
 	 * @var int DB Version
