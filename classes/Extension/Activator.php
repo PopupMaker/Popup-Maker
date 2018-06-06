be75450312d36110617a1b664ce71b6ac27f6b86
Daniel Iser <danieliser@wizardinternetsolutions.com>
2018-06-06 01:19:21 -0400

    Updated extension activator to be PHP 5.2 compatible.

--- classes/Extension/Activator.php
+++ classes/Extension/Activator.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Popup Maker Extension Activation Handler Class
  *
- * @since       1.0.0
+ * @version       1.1.0
  */
 class PUM_Extension_Activator {
 
@@ -27,6 +27,21 @@ class PUM_Extension_Activator {
 	 */
 	public $extension_slug;
 
+	/**
+	 * @var int
+	 */
+	public $extension_id;
+
+	/**
+	 * @var string
+	 */
+	public $extension_version;
+
+	/**
+	 * @var string
+	 */
+	public $extension_file;
+
 	public $required_core_version;
 
 	/**
@@ -35,6 +50,26 @@ class PUM_Extension_Activator {
 	public $core_installed = false;
 
 	public $core_path;
+
+	/**
+	 * @param $class_name
+	 * @param $prop_name
+	 *
+	 * @return bool|mixed
+	 */
+	public function get_static_prop( $class_name, $prop_name ) {
+		if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+			try {
+				$class = new ReflectionClass( $class_name );
+
+				return $class->getStaticPropertyValue( $prop_name );
+			} catch ( ReflectionException $e ) {
+				return false;
+			}
+		}
+
+		return $class_name::$$prop_name;
+	}
 
 	/**
 	 * Setup the activator class
@@ -56,8 +91,10 @@ class PUM_Extension_Activator {
 		}
 
 		$this->extension_class_name  = $class_name;
-		$this->extension_name        = $class_name::$NAME;
-		$this->required_core_version = $class_name::$REQUIRED_CORE_VER;
+		$this->extension_id          = $this->get_static_prop( $class_name, 'ID' );
+		$this->extension_name        = $this->get_static_prop( $class_name, 'NAME' );
+		$this->extension_version     = $this->get_static_prop( $class_name, 'VER' );
+		$this->required_core_version = $this->get_static_prop( $class_name, 'REQUIRED_CORE_VER' );
 
 		$plugins = get_plugins();
 
@@ -98,14 +135,18 @@ class PUM_Extension_Activator {
 			add_action( 'admin_notices', array( $this, 'missing_popmake_notice' ) );
 		} else {
 			$class_name = $this->extension_class_name;
-			$class_name::instance();
 
-			$plugin_slug          = explode( '/', plugin_basename( $class_name::$FILE ), 2 );
+			// Generate an instance of the extension class in a PHP 5.2 compatible way.
+			call_user_func( array( $class_name, 'instance' ) );
+
+			$this->extension_file = $this->get_static_prop( $class_name, 'FILE' );
+
+			$plugin_slug          = explode( '/', plugin_basename( $this->extension_file ), 2 );
 			$this->extension_slug = str_replace( array( 'popup-maker-', 'pum-' ), '', $plugin_slug[0] );
 
 			// Handle licensing
 			if ( class_exists( 'PUM_Extension_License' ) ) {
-				new PUM_Extension_License( $class_name::$FILE, $class_name::$NAME, $class_name::$VER, 'WP Popup Maker', null, null, $class_name::$ID );
+				new PUM_Extension_License( $this->extension_file, $this->extension_name, $this->extension_version, 'WP Popup Maker', null, null, $this->extension_id );
 			}
 
 			add_filter( 'pum_enabled_extensions', array( $this, 'enabled_extensions' ) );
