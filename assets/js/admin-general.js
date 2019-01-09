d7d8df3e3ba98ddd5c2d96843ee6298df4c18c98
Daniel Iser <danieliser@wizardinternetsolutions.com>
2019-01-08 23:36:32 -0500

    Added alert JS assets.

--- assets/js/admin-general.js
+++ assets/js/admin-general.js
@@ -6296,6 +6296,56 @@
 (function ($) {
     "use strict";
 
+    function dismiss(alert, reason) {
+        $.ajax({
+            method: "POST",
+            dataType: "json",
+            url: ajaxurl,
+            data: {
+                action: 'pum_alerts_action',
+                nonce: window.pum_alerts_nonce,
+                code: alert.data('code')
+            }
+        });
+    }
+
+    var $alerts = $('.pum-alerts'),
+        $notice_counts = $('.pum-alert-count'),
+        count = parseInt($notice_counts.eq(0).text());
+
+    $(document)
+        .on('click', '.pum-alert-holder .pum-dismiss', function () {
+            var $this = $(this),
+                alert = $this.parents('.pum-alert-holder');
+
+            count--;
+
+            $notice_counts.text(count);
+
+            alert.fadeTo(100, 0, function () {
+                alert.slideUp(100, function () {
+                    alert.remove();
+
+                    if ($alerts.find('.pum-alert-holder').length === 0) {
+                        $alerts.slideUp(100, function () {
+                            $alerts.remove();
+                        });
+
+                        $('#menu-posts-popup .wp-menu-name .update-plugins').fadeOut();
+                    }
+                });
+            });
+
+            dismiss(alert);
+        });
+
+}(jQuery));
+/*******************************************************************************
+ * Copyright (c) 2017, WP Popup Maker
+ ******************************************************************************/
+(function ($) {
+    "use strict";
+
     var colorpicker = {
         init: function () {
             $('.pum-color-picker').filter(':not(.pum-color-picker-initialized)')
