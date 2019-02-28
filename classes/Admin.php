f7a0f01493568a2e4e6a00d82f36b53d450696fa
Daniel Iser <danieliser@wizardinternetsolutions.com>
2019-02-28 01:48:03 -0500

    Added translation request link to plugin action links.

--- classes/Admin.php
+++ classes/Admin.php
@@ -52,9 +52,14 @@ class PUM_Admin {
 
 		if ( $file == plugin_basename( POPMAKE ) ) {
 			$plugin_action_links = apply_filters( 'pum_plugin_action_links', array(
-				'extend'   => '<a href="' . admin_url( 'edit.php?post_type=popup&page=pum-extensions' ) . '">' . __( 'Integrations', 'popup-maker' ) . '</a>',
-				'settings' => '<a href="' . admin_url( 'edit.php?post_type=popup&page=pum-settings' ) . '">' . __( 'Settings', 'popup-maker' ) . '</a>',
+				'extend'    => '<a href="' . admin_url( 'edit.php?post_type=popup&page=pum-extensions' ) . '">' . __( 'Integrations', 'popup-maker' ) . '</a>',
+				'settings'  => '<a href="' . admin_url( 'edit.php?post_type=popup&page=pum-settings' ) . '">' . __( 'Settings', 'popup-maker' ) . '</a>',
 			) );
+
+			// TODO Rewrite this to take full advantage of our polyglot detection code in Alerts for translation requests.
+			if ( substr( get_locale(), 0, 2 ) != 'en' ) {
+				$plugin_action_links = array_merge( array( 'translate' => '<a href="' . sprintf( 'https://translate.wordpress.org/locale/%s/default/wp-plugins/popup-maker', substr( get_locale(), 0, 2 ) ) . '" target="_blank">' . __( 'Translate', 'popup-maker' ) . '</a>' ), $plugin_action_links );
+			}
 
 			foreach ( $plugin_action_links as $link ) {
 				array_unshift( $links, $link );
