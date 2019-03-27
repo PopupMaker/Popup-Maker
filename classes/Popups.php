530a8bcbd0f4e57089472e28eec2ee2ce7d5c67e
Daniel Iser <danieliser@wizardinternetsolutions.com>
2019-03-27 00:20:29 -0400

    Removed unused code.

--- classes/Popups.php
+++ classes/Popups.php
@@ -17,17 +17,6 @@ class PUM_Popups {
 	 * Hook the initialize method to the WP init action.
 	 */
 	public static function init() {
-		add_filter( 'pum_popup_content', array( $GLOBALS['wp_embed'], 'run_shortcode' ), 8 );
-		add_filter( 'pum_popup_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
-		add_filter( 'pum_popup_content', 'wptexturize', 10 );
-		add_filter( 'pum_popup_content', 'convert_smilies', 10 );
-		add_filter( 'pum_popup_content', 'convert_chars', 10 );
-		add_filter( 'pum_popup_content', 'wpautop', 10 );
-		add_filter( 'pum_popup_content', 'shortcode_unautop', 10 );
-		add_filter( 'pum_popup_content', 'prepend_attachment', 10 );
-		add_filter( 'pum_popup_content', 'force_balance_tags', 10 );
-		add_filter( 'pum_popup_content', array( 'PUM_Helpers', 'do_shortcode' ), 11 );
-		add_filter( 'pum_popup_content', 'capital_P_dangit', 11 );
 	}
 
 
