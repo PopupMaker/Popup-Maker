b09b51b80404b243bf0318a545ab6475c2bb49fb
danieliser <danieliser@wizardinternetsolutions.com>
2019-06-29 22:27:03 -0400

    Fixed typo in setting current_popup

--- classes/Site/Popups.php
+++ classes/Site/Popups.php
@@ -99,7 +99,7 @@ class PUM_Site_Popups {
 
 			foreach ( $popups as $popup ) {
 				// Set this popup as the global $current.
-				pum()->current_theme = $popup;
+				pum()->current_popup = $popup;
 
 				// If the popup is loadable (passes conditions) load it.
 				if ( pum_is_popup_loadable( $popup->ID ) ) {
