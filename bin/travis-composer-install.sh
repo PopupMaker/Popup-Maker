7fe1234ae766deae533287173d90a616568f5227
Kyle Johnson <kyle@saturdaydrive.io>
2019-10-11 19:05:18 -0400

    Update PHP version compare to account for version number format.

--- bin/travis-composer-install.sh
+++ bin/travis-composer-install.sh
@@ -1,6 +1,9 @@
-if [ TRAVIS_PHP_VERSION >= 7 ]
-then
-    COMPOSER=composer-travis-php7.json composer install
+#!/usr/bin/env bash
+
+function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+if [ TRAVIS_PHP_VERSION = 7 ] || version_gt TRAVIS_PHP_VERSION 7; then
+     COMPOSER=composer-travis-php7.json composer install
 else
     COMPOSER=composer-travis-php5.json composer install
 fi
