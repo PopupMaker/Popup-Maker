94224fca48925e8f543ec260ad1e933859146e8e
danieliser <danieliser@wizardinternetsolutions.com>
2020-01-07 04:33:34 -0500

    Check that we only detect complete submissions

--- assets/js/src/integration/calderaforms.js
+++ assets/js/src/integration/calderaforms.js
@@ -5,22 +5,34 @@
 {
 	const formProvider = 'calderaforms';
 	const $ = window.jQuery;
+	let $form;
+
+	/**
+	 * This function is run before every CF Ajax call to store the form being submitted.
+	 *
+	 * @param event
+	 * @param obj
+	 */
+	const beforeAjax = (event, obj) => $form = obj.$form;
 
 	$(document)
-		.on('cf.form.submit', function (event, data) {
-			//data.$form is a jQuery object for the form that just submitted.
-			const $form = data.$form;
-			//get the form that is submiting's ID attribute
-			const [ formId, formInstanceId ] = $form.attr('id').split('_');
+		.on('cf.ajax.request', beforeAjax)
+		// After all requests
+		.on('cf.submission', function (event, obj) {
+			// Only if status of request is complete|success.
+			if ("complete" === obj.data.status || "success" === obj.data.status) {
+				//get the form that is submiting's ID attribute
+				const [formId, formInstanceId = null] = $form.attr('id').split('_');
 
-			// All the magic happens here.
-			window.PUM.integrations.formSubmission($form[0], {
-				formProvider,
-				formId,
-				formInstanceId,
-				extras: {
-					state: window.cfstate.hasOwnProperty( formId ) ? window.cfstate[formId] : null
-				}
-			});
+				// All the magic happens here.
+				window.PUM.integrations.formSubmission($form, {
+					formProvider,
+					formId,
+					formInstanceId,
+					extras: {
+						state: window.cfstate.hasOwnProperty(formId) ? window.cfstate[formId] : null
+					}
+				});
+			}
 		});
 }
