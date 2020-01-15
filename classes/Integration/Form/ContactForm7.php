06305073189aa2d027a34a256659c8fff0f82b47
danieliser <danieliser@wizardinternetsolutions.com>
2020-01-15 03:18:49 -0500

    Updated inline documentation
    Updated documentation further so this could be used as the example going
    forward.

--- classes/Integration/Form/ContactForm7.php
+++ classes/Integration/Form/ContactForm7.php
@@ -6,22 +6,33 @@
 class PUM_Integration_Form_ContactForm7 extends PUM_Abstract_Integration_Form {
 
 	/**
+	 * Unique key identifier for this provider.
+	 *
 	 * @var string
 	 */
 	public $key = 'contactform7';
 
+	/**
+	 * Only used to hook in a custom action for non AJAX based submissions.
+	 *
+	 * Could be used for other initiations as well where needed.
+	 */
 	public function __construct() {
 		add_action( 'wpcf7_mail_sent', array( $this, 'on_success' ), 1 );
 	}
 
 	/**
+	 * Text label that will be used throughout the various options screens.
+	 *
 	 * @return string
 	 */
 	public function label() {
-		return 'Contact Form 7';
+		return __( 'Contact Form 7' );
 	}
 
 	/**
+	 * Should return true when the required form plugin is active.
+	 *
 	 * @return bool
 	 */
 	public function enabled() {
@@ -29,6 +40,8 @@ class PUM_Integration_Form_ContactForm7 extends PUM_Abstract_Integration_Form {
 	}
 
 	/**
+	 * Return a useable array of all forms from this provider.
+	 *
 	 * @return array
 	 */
 	public function get_forms() {
@@ -39,6 +52,8 @@ class PUM_Integration_Form_ContactForm7 extends PUM_Abstract_Integration_Form {
 	}
 
 	/**
+	 * Return a single form by ID.
+	 *
 	 * @param string $id
 	 *
 	 * @return mixed
@@ -48,6 +63,10 @@ class PUM_Integration_Form_ContactForm7 extends PUM_Abstract_Integration_Form {
 	}
 
 	/**
+	 * Returns an array of options for a select list.
+	 *
+	 * Should be in the format of $formId => $formLabel
+	 *
 	 * @return array
 	 */
 	public function get_form_selectlist() {
@@ -63,9 +82,14 @@ class PUM_Integration_Form_ContactForm7 extends PUM_Abstract_Integration_Form {
 	}
 
 	/**
+	 * Hooks in a success functions specific to this provider for non AJAX submission handling.
+	 *
 	 * @param WPCF7_ContactForm $cfdata
 	 */
 	public function on_success( $cfdata ) {
+		/**
+		 * @see pum_integrated_form_submission
+		 */
 		pum_integrated_form_submission( [
 			'popup_id'      => isset( $_REQUEST['pum_form_popup_id'] ) && absint( $_REQUEST['pum_form_popup_id'] ) > 0 ? absint( $_REQUEST['pum_form_popup_id'] ) : false,
 			'form_provider' => $this->key,
@@ -74,12 +98,14 @@ class PUM_Integration_Form_ContactForm7 extends PUM_Abstract_Integration_Form {
 	}
 
 	/**
+	 * Load a custom script file to handle AJAX based submissions or other integrations with Popup Maker frontend.
+	 *
 	 * @param array $js
 	 *
 	 * @return array
 	 */
 	public function custom_scripts( $js = [] ) {
-		$js['contactform7'] = [
+		$js[ $this->key ] = [
 			'content'  => file_get_contents( Popup_Maker::$DIR . 'assets/js/pum-integration-contactform7.js' ),
 			'priority' => 8,
 		];
@@ -88,6 +114,8 @@ class PUM_Integration_Form_ContactForm7 extends PUM_Abstract_Integration_Form {
 	}
 
 	/**
+	 * Load custom styles for hacking some elements specifically inside popups, such as datepickers.
+	 *
 	 * @param array $css
 	 *
 	 * @return array
