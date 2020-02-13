521c21ae262991af499a6b77a5adcb032ded4375
danieliser <danieliser@wizardinternetsolutions.com>
2020-02-13 03:30:59 -0500

    Fixed code formatting

--- assets/js/src/integration/calderaforms.js
+++ assets/js/src/integration/calderaforms.js
@@ -13,26 +13,26 @@
 	 * @param event
 	 * @param obj
 	 */
-	const beforeAjax = (event, obj) => $form = obj.$form;
+	const beforeAjax = ( event, obj ) => $form = obj.$form;
 
-	$(document)
-		.on('cf.ajax.request', beforeAjax)
+	$( document )
+		.on( 'cf.ajax.request', beforeAjax )
 		// After all requests
-		.on('cf.submission', function (event, obj) {
+		.on( 'cf.submission', function ( event, obj ) {
 			// Only if status of request is complete|success.
-			if ("complete" === obj.data.status || "success" === obj.data.status) {
+			if ( 'complete' === obj.data.status || 'success' === obj.data.status ) {
 				//get the form that is submiting's ID attribute
-				const [formId, formInstanceId = null] = $form.attr('id').split('_');
+				const [ formId, formInstanceId = null ] = $form.attr( 'id' ).split( '_' );
 
 				// All the magic happens here.
-				window.PUM.integrations.formSubmission($form, {
+				window.PUM.integrations.formSubmission( $form, {
 					formProvider,
 					formId,
 					formInstanceId,
 					extras: {
-						state: window.cfstate.hasOwnProperty(formId) ? window.cfstate[formId] : null
-					}
-				});
+						state: window.cfstate.hasOwnProperty( formId ) ? window.cfstate[ formId ] : null,
+					},
+				} );
 			}
-		});
+		} );
 }
