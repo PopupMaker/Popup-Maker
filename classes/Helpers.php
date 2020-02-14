6fce83c34efc6b7160f915132d3b899eaac66254
Frank Corso <frank@frankcorso.me>
2020-02-14 16:06:46 -0500

    Improve getting uploads directory for generating and enqueueing cache
    We needed to add in a few checks to ensure the uploads directory system
    was working as well as making sure no errors were coming out of it. So,
    went ahead and rewrote some of the functions by creating 3 new helpers
    as well as moved the main calls to each into the variable in the assets
    and assetcache classed and tested against those instead of rerunning
    the functions again as it currently did.

--- classes/Helpers.php
+++ classes/Helpers.php
@@ -53,6 +53,72 @@ class PUM_Helpers {
 		return $shortcodes;
 	}
 
+	/**
+	 * Gets the uploads directory path
+	 *
+	 * @since 1.10
+	 * @param string $path A path to append to end of upload directory URL.
+	 * @return bool|string The uploads directory path or false on failure
+	 */
+	public static function get_upload_dir_path( $path = '' ) {
+		$upload_dir = self::get_upload_dir();
+		if ( false !== $upload_dir && isset( $upload_dir['basedir'] ) ) {
+			$dir = $upload_dir['basedir'];
+			if ( ! empty( $path ) ) {
+				$dir = trailingslashit( $dir ) . $path;
+			}
+			return $dir;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Gets the uploads directory URL
+	 *
+	 * @since 1.10
+	 * @param string $path A path to append to end of upload directory URL.
+	 * @return bool|string The uploads directory URL or false on failure
+	 */
+	public static function get_upload_dir_url( $path = '' ) {
+		$upload_dir = self::get_upload_dir();
+		if ( false !== $upload_dir && isset( $upload_dir['baseurl'] ) ) {
+			$url = preg_replace( '/^https?:/', '', $upload_dir['baseurl'] );
+			if ( null === $url ) {
+				return false;
+			}
+			if ( ! empty( $path ) ) {
+				$url = trailingslashit( $url ) . $path;
+			}
+			return $url;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Gets the Uploads directory
+	 *
+	 * @since 1.10
+	 * @return bool|array An associated array with baseurl and basedir or false on failure
+	 */
+	public static function get_upload_dir() {
+		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+			$wp_upload_dir = wp_get_upload_dir();
+		} else {
+			$wp_upload_dir = wp_upload_dir();
+		}
+
+		if ( isset( $wp_upload_dir['error'] ) ) {
+			return false;
+		} else {
+			return $wp_upload_dir;
+		}
+	}
+
+	/**
+	 * @deprecated Use get_upload_dir_url instead.
+	 */
 	public static function upload_dir_url( $path = '' ) {
 		$upload_dir = wp_upload_dir();
 		$upload_dir = $upload_dir['baseurl'];
