66d10e713e0d8bd519454c591fa52cedf0ce5fa2
danieliser <danieliser@wizardinternetsolutions.com>
2020-04-02 22:28:05 -0400

    Change label to not be so specific

--- classes/Cookies.php
+++ classes/Cookies.php
@@ -111,7 +111,7 @@ class PUM_Cookies {
 				'name' => __( 'Subscription Form: Already Subscribed', 'popup-maker' ),
 			),
 			'manual'                          => array(
-				'name' => __( 'Manual JavaScript', 'popup-maker' ),
+				'name' => __( 'Manual', 'popup-maker' ),
 				'settings_column' => '<pre><code>[popup_cookie name="{{data.name}}" expires="{{data.time}}" sitewide="{{data.path ? 1 : 0}}"]</code></pre>',
 			),
 		) );
