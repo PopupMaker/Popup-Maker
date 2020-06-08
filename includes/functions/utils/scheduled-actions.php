c9dcdd1731daba1467b2ad26323a44ed8e7da770
danieliser <danieliser@wizardinternetsolutions.com>
2020-06-08 15:57:34 -0400

    Update scheduled action method signatures

--- includes/functions/utils/scheduled-actions.php
+++ includes/functions/utils/scheduled-actions.php
@@ -11,7 +11,7 @@
  * claimed by both the existing WP Cron and WP CLI runners, as well as a new async request runner.
  *
  * @param string $hook The hook to trigger when this action runs
- * @param array $args Args to pass when the hook is triggered
+ * @param array  $args Args to pass when the hook is triggered
  *
  * @return string The ID of the stored action
  */
@@ -22,24 +22,24 @@ function pum_enqueue_async_action( $hook = 'none', $args = [] ) {
 /**
  * Schedule an action to run one time
  *
- * @param int $timestamp Unix timestamp when the action will run
- * @param string $hook The hook to trigger
- * @param array $args Arguments to pass when the hook triggers
+ * @param int    $timestamp Unix timestamp when the action will run
+ * @param string $hook      The hook to trigger
+ * @param array  $args      Arguments to pass when the hook triggers
  *
  * @return string The job ID
  */
 function pum_schedule_single_action( $timestamp, $hook, $args = [] ) {
-	return as_schedule_single_action( $timestamp, $hook, $args = array(), 'popup-maker' );
+	return as_schedule_single_action( $timestamp, $hook, $args, 'popup-maker' );
 }
 
 /**
  * Schedule a recurring action
  * Create the first instance of an action recurring on a given interval.
  *
- * @param int $timestamp Unix timestamp when the action will run the first time
- * @param int $interval_in_seconds How long to wait between runs
- * @param string $hook The hook to trigger
- * @param array $args Arguments to pass when the hook triggers
+ * @param int    $timestamp           Unix timestamp when the action will run the first time
+ * @param int    $interval_in_seconds How long to wait between runs
+ * @param string $hook                The hook to trigger
+ * @param array  $args                Arguments to pass when the hook triggers
  *
  * @return string The job ID
  */
@@ -51,24 +51,24 @@ function pum_schedule_recurring_action( $timestamp, $interval_in_seconds, $hook,
 /**
  * Schedule an action that recurs on a cron-like schedule.
  *
- * @param int $timestamp The first instance of the action will be scheduled
- *        to run at a time calculated after this timestamp matching the cron
- *        expression. This can be used to delay the first instance of the action.
- * @param string $schedule A cron-link schedule string
- * @see http://en.wikipedia.org/wiki/Cron
- *   *    *    *    *    *    *
- *   ┬    ┬    ┬    ┬    ┬    ┬
- *   |    |    |    |    |    |
- *   |    |    |    |    |    + year [optional]
- *   |    |    |    |    +----- day of week (0 - 7) (Sunday=0 or 7)
- *   |    |    |    +---------- month (1 - 12)
- *   |    |    +--------------- day of month (1 - 31)
- *   |    +-------------------- hour (0 - 23)
- *   +------------------------- min (0 - 59)
- * @param string $hook The hook to trigger
- * @param array $args Arguments to pass when the hook triggers
+ * @param int    $timestamp The first instance of the action will be scheduled
+ *                          to run at a time calculated after this timestamp matching the cron
+ *                          expression. This can be used to delay the first instance of the action.
+ * @param string $schedule  A cron-link schedule string
+ * @param string $hook      The hook to trigger
+ * @param array  $args      Arguments to pass when the hook triggers
  *
  * @return string The job ID
+ * @see http://en.wikipedia.org/wiki/Cron
+ *      *    *    *    *    *    *
+ *      ┬    ┬    ┬    ┬    ┬    ┬
+ *      |    |    |    |    |    |
+ *      |    |    |    |    |    + year [optional]
+ *      |    |    |    |    +----- day of week (0 - 7) (Sunday=0 or 7)
+ *      |    |    |    +---------- month (1 - 12)
+ *      |    |    +--------------- day of month (1 - 31)
+ *      |    +-------------------- hour (0 - 23)
+ *      +------------------------- min (0 - 59)
  */
 function pum_schedule_cron_action( $timestamp, $schedule, $hook, $args = [] ) {
 	return as_schedule_cron_action( $timestamp, $schedule, $hook, $args, 'popup-maker' );
@@ -85,7 +85,7 @@ function pum_schedule_cron_action( $timestamp, $schedule, $hook, $args = [] ) {
  * by this method also.
  *
  * @param string $hook The hook that the job will trigger
- * @param array $args Args that would have been passed to the job
+ * @param array  $args Args that would have been passed to the job
  *
  * @return string The scheduled action ID if a scheduled action was found, or empty string if no matching action found.
  */
@@ -97,12 +97,36 @@ function pum_unschedule_action( $hook, $args = [] ) {
  * Cancel all occurrences of a scheduled action.
  *
  * @param string $hook The hook that the job will trigger
- * @param array $args Args that would have been passed to the job
+ * @param array  $args Args that would have been passed to the job
  *
  * @return void
  */
-function pum_unschedule_all_actions( $hook, $args = [] ) {
-	as_unschedule_all_actions( $hook, $args, 'popup-maker' );
+function pum_unschedule_all_actions_by_hook( $hook ) {
+	as_unschedule_all_actions( $hook, null );
+}
+
+/**
+ * Cancel all occurrences of a scheduled action.
+ *
+ * @param string $hook The hook that the job will trigger
+ * @param array  $args Args that would have been passed to the job
+ *
+ * @return void
+ */
+function pum_unschedule_all_group_actions() {
+	as_unschedule_all_actions( null, null, 'popup-maker' );
+}
+
+/**
+ * Cancel all occurrences of a scheduled action.
+ *
+ * @param string $hook The hook that the job will trigger
+ * @param array  $args Args that would have been passed to the job
+ *
+ * @return void
+ */
+function pum_unschedule_all_actions( $hook, $args = [], $group_only = false ) {
+	as_unschedule_all_actions( $hook, $args, $group_only ? 'popup-maker' : '' );
 }
 
 /**
@@ -115,31 +139,31 @@ function pum_unschedule_all_actions( $hook, $args = [] ) {
  * boolean false will be the return value.
  *
  * @param string $hook
- * @param array $args
+ * @param array  $args
  *
  * @return int|bool The timestamp for the next occurrence of a pending scheduled action, true for an async or in-progress action or false if there is no matching action.
  */
-function pum_next_scheduled_action( $hook, $args = NULL ) {
+function pum_next_scheduled_action( $hook, $args = [] ) {
 	return as_next_scheduled_action( $hook, $args, 'popup-maker' );
 }
 
 /**
  * Find scheduled actions
  *
- * @param array $args Possible arguments, with their default values:
- *        'hook' => '' - the name of the action that will be triggered
- *        'args' => NULL - the args array that will be passed with the action
- *        'date' => NULL - the scheduled date of the action. Expects a DateTime object, a unix timestamp, or a string that can parsed with strtotime(). Used in UTC timezone.
- *        'date_compare' => '<=' - operator for testing "date". accepted values are '!=', '>', '>=', '<', '<=', '='
- *        'modified' => NULL - the date the action was last updated. Expects a DateTime object, a unix timestamp, or a string that can parsed with strtotime(). Used in UTC timezone.
- *        'modified_compare' => '<=' - operator for testing "modified". accepted values are '!=', '>', '>=', '<', '<=', '='
- *        'group' => '' - the group the action belongs to
- *        'status' => '' - ActionScheduler_Store::STATUS_COMPLETE or ActionScheduler_Store::STATUS_PENDING
- *        'claimed' => NULL - TRUE to find claimed actions, FALSE to find unclaimed actions, a string to find a specific claim ID
- *        'per_page' => 5 - Number of results to return
- *        'offset' => 0
- *        'orderby' => 'date' - accepted values are 'hook', 'group', 'modified', or 'date'
- *        'order' => 'ASC'
+ * @param array  $args          Possible arguments, with their default values:
+ *                              'hook' => '' - the name of the action that will be triggered
+ *                              'args' => NULL - the args array that will be passed with the action
+ *                              'date' => NULL - the scheduled date of the action. Expects a DateTime object, a unix timestamp, or a string that can parsed with strtotime(). Used in UTC timezone.
+ *                              'date_compare' => '<=' - operator for testing "date". accepted values are '!=', '>', '>=', '<', '<=', '='
+ *                              'modified' => NULL - the date the action was last updated. Expects a DateTime object, a unix timestamp, or a string that can parsed with strtotime(). Used in UTC timezone.
+ *                              'modified_compare' => '<=' - operator for testing "modified". accepted values are '!=', '>', '>=', '<', '<=', '='
+ *                              'group' => '' - the group the action belongs to
+ *                              'status' => '' - ActionScheduler_Store::STATUS_COMPLETE or ActionScheduler_Store::STATUS_PENDING
+ *                              'claimed' => NULL - TRUE to find claimed actions, FALSE to find unclaimed actions, a string to find a specific claim ID
+ *                              'per_page' => 5 - Number of results to return
+ *                              'offset' => 0
+ *                              'orderby' => 'date' - accepted values are 'hook', 'group', 'modified', or 'date'
+ *                              'order' => 'ASC'
  *
  * @param string $return_format OBJECT, ARRAY_A, or ids
  *
