0818f46a5bf33ee27499ee05d677ec354c64a2ec
Frank Corso <frank@frankcorso.me>
2020-08-18 12:16:26 -0400

    Change UTM parameters on admin bar help link to align with other contextual help links

--- includes/modules/admin-bar.php
+++ includes/modules/admin-bar.php
@@ -162,7 +162,7 @@ class PUM_Modules_Admin_Bar {
 			$wp_admin_bar->add_node( array(
 				'id'     => 'no-popups-loaded',
 				'title'  => __( 'No Popups Loaded', 'popup-maker' ) . '<strong style="color:#fff; margin-left: 5px;">?</strong>',
-				'href'   => 'https://docs.wppopupmaker.com/article/265-my-popup-wont-work-how-can-i-fix-it?utm_capmaign=Self+Help&utm_source=No+Popups&utm_medium=Admin+Bar',
+				'href'   => 'https://docs.wppopupmaker.com/article/265-my-popup-wont-work-how-can-i-fix-it?utm_campaign=contextual-help&utm_medium=inline-doclink&utm_source=plugin-admin-bar&utm_content=no-popups-loaded',
 				'parent' => 'popups',
 				'meta'   => array(
 					'target' => '_blank',
