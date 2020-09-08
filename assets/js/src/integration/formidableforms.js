f99ea2045228a383342d2343bb66ef84cb1c5d93
Frank Corso <frank@frankcorso.me>
2020-09-08 13:43:07 -0400

    Fix ESLint warnings

--- assets/js/src/integration/formidableforms.js
+++ assets/js/src/integration/formidableforms.js
@@ -6,19 +6,17 @@
 	const formProvider = "formidableforms";
 	const $ = window.jQuery;
 
-	$(document).ready(() => {
-		$(document).on("frmFormComplete", function(event, form, response) {
-			var $form = $(form);
-			var formId = $form.find('input[name="form_id"]').val();
+	$(document).on("frmFormComplete", function(event, form, response) {
+		const $form = $(form);
+		const formId = $form.find('input[name="form_id"]').val();
 
-			// All the magic happens here.
-			window.PUM.integrations.formSubmission($form, {
-				formProvider,
-				formId,
-				extras: {
-					response
-				}
-			});
+		// All the magic happens here.
+		window.PUM.integrations.formSubmission($form, {
+			formProvider,
+			formId,
+			extras: {
+				response
+			}
 		});
 	});
 }
