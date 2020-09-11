c84162f4a9eb24db4f75c0304ae8d9b0b2d87450
Frank Corso <frank@frankcorso.me>
2020-09-11 11:25:17 -0400

    Wraps up concept function for sending active state to new AJAX endpoint
    Issue #544

--- assets/js/src/admin/general/general.js
+++ assets/js/src/admin/general/general.js
@@ -3,20 +3,38 @@
  ********************************************/
 
 (function($) {
+	/**
+	 * Changes the current active state of supplied popup
+	 *
+	 * @param {number} popupID The ID for the popup.
+	 * @param {number} activeState 1 for active, 0 for inactive.
+	 */
+	function changeActiveState(popupID, activeState) {
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			// eslint-disable-next-line no-undef
+			url: ajaxurl,
+			data: {
+				action: "pum_save_active_state",
+				nonce: "",
+				popupID: popupID,
+				active: activeState
+			}
+		});
+	}
+
 	$(function() {
 		$(".pum-active-toggle-button").on("click", function(e) {
 			e.preventDefault();
-			var popupID = $(this).data("popup-id");
-			$.ajax({
-				type: "POST",
-				dataType: "json",
-				url: pum_vars.ajaxurl,
-				data: {
-					action: "pum_save_active_state",
-					popupID: popupID,
-					active: 0,
-				}
-			});
+			var $button = $(this);
+			var currentState = $button.data("active-state");
+			var newState = 1;
+			if (1 === parseInt(currentState)) {
+				newState = 0;
+			}
+			changeActiveState($button.data("popup-id"), newState);
+			$button.data("active-state", newState);
 		});
 	});
 })(jQuery);
