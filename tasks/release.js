4c2b24f5cc4238f933491c2be4c62c857ceaefc1
Frank Corso <frank@frankcorso.me>
2020-09-22 13:18:23 -0400

    Convert more instances of "WP Popup Maker" to "Popup Maker"

--- tasks/release.js
+++ tasks/release.js
@@ -1,13 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2019, WP Popup Maker
- ******************************************************************************/
-require('./package');
-require('./clean');
+/**************************************
+ * Copyright (c) 2020, Popup Maker
+ *************************************/
+require("./package");
+require("./clean");
 
-const gulp = require('gulp');
+const gulp = require("gulp");
 
-gulp.task('release', gulp.series('build', 'package', 'clean_build'));
+gulp.task("release", gulp.series("build", "package", "clean_build"));
 
-let release = gulp.task('release');
+let release = gulp.task("release");
 
 release.description = "Runs all build routines and generates a release.";
