20dc2348129e18de354663455a34154c8f258fd4
Frank Corso <frank@frankcorso.me>
2020-10-06 08:49:37 -0400

    Create initial tests for utils array class
    Issue: #563

--- tests/test-pum_admin_onboarding.php
+++ tests/test-pum_admin_onboarding.php
@@ -5,18 +5,17 @@
  * @package Popup_Maker
  */
 
-use PHPUnit\Framework\TestCase;
 
 /**
  * Test methods within our PUM_Admin_Onboarding class
  */
-class TestOnboarding extends WP_UnitTestCase {
+class PUM_Admin_OnboardingTEST extends WP_UnitTestCase {
 
 	/**
 	 * Tests to make sure data returned from `all_popups_main_tour` is valid.
 	 */
 	public function test_all_popups_pointers() {
 		$pointers = PUM_Admin_Onboarding::all_popups_main_tour( array() );
-		$this->assertTrue( is_array( $pointers ) );
+		$this->assertIsArray( $pointers );
 	}
 }
