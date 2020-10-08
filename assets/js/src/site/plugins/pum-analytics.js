371c4b28c82331613763db82a6a0dcfb0374afe9
Frank Corso <frank@frankcorso.me>
2020-10-08 13:41:20 -0400

    Add conversion function in analytics js hooked to form success
    Issue #775

--- assets/js/src/site/plugins/pum-analytics.js
+++ assets/js/src/site/plugins/pum-analytics.js
@@ -4,63 +4,99 @@
  */
 
 var PUM_Analytics;
-(function ($) {
-    "use strict";
+(function($) {
+	"use strict";
 
-    $.fn.popmake.last_open_trigger = null;
-    $.fn.popmake.last_close_trigger = null;
-    $.fn.popmake.conversion_trigger = null;
+	$.fn.popmake.last_open_trigger = null;
+	$.fn.popmake.last_close_trigger = null;
+	$.fn.popmake.conversion_trigger = null;
 
-    var rest_enabled = !!(typeof pum_vars.restapi !== 'undefined' && pum_vars.restapi);
+	var rest_enabled = !!(
+		typeof pum_vars.restapi !== "undefined" && pum_vars.restapi
+	);
 
-    PUM_Analytics = {
-        beacon: function (data, callback) {
-            var beacon = new Image(),
-                url = rest_enabled ? pum_vars.restapi : pum_vars.ajaxurl,
-                opts = {
-                    route: pum.hooks.applyFilters( 'pum.analyticsBeaconRoute', '/analytics/' ),
-                    data: pum.hooks.applyFilters( 'pum.AnalyticsBeaconData', $.extend( true, {
-                        event: 'open',
-                        pid: null,
-                        _cache: (+(new Date()))
-                    }, data ) ),
-                    callback: typeof callback === 'function' ? callback : function () {
-                    }
-                };
+	PUM_Analytics = {
+		beacon: function(data, callback) {
+			var beacon = new Image(),
+				url = rest_enabled ? pum_vars.restapi : pum_vars.ajaxurl,
+				opts = {
+					route: pum.hooks.applyFilters(
+						"pum.analyticsBeaconRoute",
+						"/analytics/"
+					),
+					data: pum.hooks.applyFilters(
+						"pum.AnalyticsBeaconData",
+						$.extend(
+							true,
+							{
+								event: "open",
+								pid: null,
+								_cache: +new Date()
+							},
+							data
+						)
+					),
+					callback:
+						typeof callback === "function"
+							? callback
+							: function() {}
+				};
 
-            if (!rest_enabled) {
-                opts.data.action = 'pum_analytics';
-            } else {
-                url += opts.route;
-            }
+			if (!rest_enabled) {
+				opts.data.action = "pum_analytics";
+			} else {
+				url += opts.route;
+			}
 
-            // Create a beacon if a url is provided
-            if (url) {
-                // Attach the event handlers to the image object
-                $(beacon).on('error success load done', opts.callback);
+			// Create a beacon if a url is provided
+			if (url) {
+				// Attach the event handlers to the image object
+				$(beacon).on("error success load done", opts.callback);
 
-                // Attach the src for the script call
-                beacon.src = url + '?' + $.param(opts.data);
-            }
-        }
-    };
+				// Attach the src for the script call
+				beacon.src = url + "?" + $.param(opts.data);
+			}
+		}
+	};
 
-    if (typeof pum_vars.disable_tracking === 'undefined' || !pum_vars.disable_tracking) {
-        // Only popups from the editor should fire analytics events.
-        $(document)
-        /**
-         * Track opens for popups.
-         */
-            .on('pumAfterOpen.core_analytics', '.pum', function () {
-                var $popup = PUM.getPopup(this),
-                    data = {
-                        pid: parseInt($popup.popmake('getSettings').id, 10) || null
-                    };
+	if (
+		typeof pum_vars.disable_tracking === "undefined" ||
+		!pum_vars.disable_tracking
+	) {
+		// Only popups from the editor should fire analytics events.
+		$(document)
+			/**
+			 * Track opens for popups.
+			 */
+			.on("pumAfterOpen.core_analytics", ".pum", function() {
+				var $popup = PUM.getPopup(this),
+					data = {
+						pid:
+							parseInt($popup.popmake("getSettings").id, 10) ||
+							null
+					};
 
-                // Shortcode popups use negative numbers, and single-popup (preview mode) shouldn't be tracked.
-                if (data.pid > 0 && !$('body').hasClass('single-popup')) {
-                    PUM_Analytics.beacon(data);
-                }
-            });
-    }
-}(jQuery));
+				// Shortcode popups use negative numbers, and single-popup (preview mode) shouldn't be tracked.
+				if (data.pid > 0 && !$("body").hasClass("single-popup")) {
+					PUM_Analytics.beacon(data);
+				}
+			})
+			/**
+			 * Track form submission conversions
+			 */
+			.on("pum.integration.form.success", function() {
+				var $popup = PUM.getPopup(this),
+					data = {
+						pid:
+							parseInt($popup.popmake("getSettings").id, 10) ||
+							null,
+						event: "conversion"
+					};
+
+				// Shortcode popups use negative numbers, and single-popup (preview mode) shouldn't be tracked.
+				if (data.pid > 0 && !$("body").hasClass("single-popup")) {
+					PUM_Analytics.beacon(data);
+				}
+			});
+	}
+})(jQuery);
