751a4e86b7e96e824cba75c16078cbb315664d55
Frank Corso <frank@frankcorso.me>
2020-11-18 11:32:20 -0500

    Add two new tests for covering PUM_Utils_Array methods

--- tests/test-pum_utils_array.php
+++ tests/test-pum_utils_array.php
@@ -28,4 +28,48 @@ class PUM_Utils_ArrayTest extends WP_UnitTestCase {
 
 		$this->assertCount( 3, $returned );
 	}
+
+	/**
+	 * Tests to make sure data returned from `remove_keys_starting_with` is valid.
+	 */
+	public function test_remove_keys_starting_with() {
+		$test = [
+			'test'  => 1,
+			'first' => 'abc'
+		];
+
+		$returned = PUM_Utils_Array::remove_keys_starting_with( $test );
+		$this->assertCount( 2, $returned );
+
+		$returned = PUM_Utils_Array::remove_keys_starting_with( $test, 'sec' );
+		$this->assertCount( 2, $returned );
+
+		$returned = PUM_Utils_Array::remove_keys_starting_with( $test, 'tes' );
+		$this->assertCount( 1, $returned );
+
+		$returned = PUM_Utils_Array::remove_keys_starting_with( $test, ['tes'] );
+		$this->assertCount( 1, $returned );
+	}
+
+	/**
+	 * Tests to make sure data returned from `remove_keys` is valid.
+	 */
+	public function test_remove_keys() {
+		$test = [
+			'test'  => 1,
+			'first' => 'abc'
+		];
+
+		$returned = PUM_Utils_Array::remove_keys( $test );
+		$this->assertCount( 2, $returned );
+
+		$returned = PUM_Utils_Array::remove_keys( $test, 'sec' );
+		$this->assertCount( 2, $returned );
+
+		$returned = PUM_Utils_Array::remove_keys( $test, 'test' );
+		$this->assertCount( 1, $returned );
+
+		$returned = PUM_Utils_Array::remove_keys( $test, ['test'] );
+		$this->assertCount( 1, $returned );
+	}
 }
