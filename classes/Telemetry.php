2aa75d444600984c68bf8007f8d0130982645443
Frank Corso <frank@frankcorso.me>
2020-12-02 11:40:35 -0500

    Set telemetry opt-in to wait 15 minutes after install
    Right now, after install, the user is prompted both the pointer tour
    and the telemtry opt-in which is both annoying and overwhelming. We
    will make the telemetry opt-in wait a few minutes to make this
    experience better.

--- classes/Telemetry.php
+++ classes/Telemetry.php
@@ -295,7 +295,7 @@ class PUM_Telemetry {
 	 * @return bool True if alert should be shown
 	 */
 	public static function should_show_alert() {
-		return false === self::has_opted_in() && current_user_can( 'manage_options' );
+		return false === self::has_opted_in() && current_user_can( 'manage_options' ) && strtotime( self::get_installed_on() . ' +15 minutes' ) < time();
 	}
 
 	/**
@@ -326,6 +326,20 @@ class PUM_Telemetry {
 	 */
 	public static function has_opted_in() {
 		return false !== pum_get_option( 'telemetry', false );
+	}
+
+	/**
+	 * Get the datetime string for when PM was installed.
+	 *
+	 * @return string
+	 * @since 1.13.0
+	 */
+	public static function get_installed_on() {
+		$installed_on = get_option( 'pum_installed_on', false );
+		if ( ! $installed_on ) {
+			$installed_on = current_time( 'mysql' );
+		}
+		return $installed_on;
 	}
 
 	/**
