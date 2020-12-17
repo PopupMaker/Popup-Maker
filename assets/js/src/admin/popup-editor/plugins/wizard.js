0203a88ade8815ee476db21ff4fc03f097c88522
Frank Corso <frank@frankcorso.me>
2020-12-17 12:28:08 -0500

    Build our more of our early prototype
    Issue #686

--- assets/js/src/admin/popup-editor/plugins/wizard.js
+++ assets/js/src/admin/popup-editor/plugins/wizard.js
@@ -8,8 +8,43 @@
 				classes: "tabbed-content",
 				cancel_button: "Skip Wizard",
 				save_button: false,
-				content: "<div><label>Search</label><input type='text'></div>"
+				content: "<div><label>Search</label><input type='text'><button type='button' class='template-selector'>Auto-open popup</button></div>"
 			})
 		);
+		$('#template-wizard').on( 'click', '.template-selector', function(e){
+			e.preventDefault();
+			PUM_Admin.modals.closeAll();
+			var $container = $(
+				"#pum-popup-settings-container"
+			);
+			var presetValues = {
+				triggers: [{
+					type: 'auto_open',
+					settings: {
+						delay: '500'
+					}
+				}],
+				cookies: [{
+					event: "on_popup_close",
+					settings: {
+						name: 'exmaple',
+						path: '1',
+						session: false,
+						time: '1 month'
+					}
+				}]
+			};
+			var args = pum_popup_settings_editor.form_args || {};
+			var originalValues = pum_popup_settings_editor.current_values || {};
+
+			var newValues = Object.assign(
+				{},
+				originalValues,
+				presetValues
+			);
+
+			// Re-render form using updated settings.
+			PUM_Admin.forms.render(args, newValues, $container);
+		});
 	});
 })(jQuery)
