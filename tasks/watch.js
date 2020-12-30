9d8b8a5b25ef26cf5580f0ebe0ac596b1626a062
Daniel Iser <daniel@code-atlantic.com>
2020-12-30 16:10:20 -0500

    Add webpack watcher & block styles entry

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,50 +2,54 @@
  * Copyright (c) 2019, WP Popup Maker
  ******************************************************************************/
 
-require('./css');
-require('./js');
-require('./langpack');
+require( './css' );
+require( './js' );
+require( './langpack' );
 
-const gulp = require('gulp'),
-	$fn = require('gulp-load-plugins')({camelize: true}),
-	path = require('path'),
-	config = require('./config.json'),
-	srcPath = path.join(config.root.dev, config.js.dev);
+const gulp = require( 'gulp' ),
+	$fn = require( 'gulp-load-plugins' )( { camelize: true } ),
+	path = require( 'path' ),
+	config = require( './config.json' ),
+	srcPath = path.join( config.root.dev, config.js.dev );
 
 function sass_watcher() {
-	$fn.saneWatch(path.join(path.join(config.root.dev, config.css.dev), '/**/*.s+(a|c)ss'), {debounce: 300}, gulp.parallel('css', 'cssrtl'));
+	$fn.saneWatch( path.join( config.root.dev, config.css.dev , '/**/*.s+(a|c)ss' ), { debounce: 300 }, gulp.parallel( 'css', 'cssrtl' ) );
 }
 
-sass_watcher.description = "Starts a scss/sass file watcher.";
+sass_watcher.description = 'Starts a scss/sass file watcher.';
 
 function js_admin_watcher() {
-	$fn.saneWatch(path.join(srcPath, 'admin', '/**/*.js'), {debounce: 300}, gulp.parallel('js_admin'));
+	$fn.saneWatch( path.join( srcPath, 'admin', '/**/*.js' ), { debounce: 300 }, gulp.parallel( 'js_admin' ) );
 }
 
-js_admin_watcher.description = "Starts admin Javascript file watcher.";
+js_admin_watcher.description = 'Starts admin Javascript file watcher.';
 
 function js_site_watcher() {
-	$fn.saneWatch(path.join(srcPath, 'site', '/**/*.js'), {debounce: 300}, gulp.parallel('js_site'));
+	$fn.saneWatch( path.join( srcPath, 'site', '/**/*.js' ), { debounce: 300 }, gulp.parallel( 'js_site' ) );
 }
+js_site_watcher.description = 'Starts site Javascript file watcher.';
 
-js_site_watcher.description = "Starts site Javascript file watcher.";
+function webpack_block_editor_watcher() {
+	$fn.saneWatch( path.join( config.root.dir, 'src/block-editor', '/**/*.js' ), { debounce: 300 }, gulp.parallel( 'webpack:blockEditor' ) );
+	$fn.saneWatch( path.join( config.root.dir, 'src/block-editor', '/**/*.s+(a|c)ss' ), { debounce: 300 }, gulp.parallel( 'webpack:blockEditor' ) );
+}
 
 function langpack_watcher() {
-	$fn.saneWatch('**/*.php', {debounce: 300}, gulp.parallel('langpack'));
+	$fn.saneWatch( '**/*.php', { debounce: 300 }, gulp.parallel( 'langpack' ) );
 }
 
-langpack_watcher.description = "Starts langpack php file watcher.";
+langpack_watcher.description = 'Starts langpack php file watcher.';
 
-gulp.task(sass_watcher);
-gulp.task(js_admin_watcher);
-gulp.task(js_site_watcher);
-gulp.task(langpack_watcher);
-gulp.task('js_watcher', gulp.parallel(['js_admin_watcher', 'js_site_watcher']));
-gulp.task('watch', gulp.parallel(['sass_watcher', 'js_watcher', 'langpack_watcher']));
+gulp.task( sass_watcher );
+gulp.task( js_admin_watcher );
+gulp.task( js_site_watcher );
+gulp.task( webpack_block_editor_watcher );
+gulp.task( langpack_watcher );
+gulp.task( 'js_watcher', gulp.parallel( [ 'js_admin_watcher', 'js_site_watcher' ] ) );
+gulp.task( 'watch', gulp.parallel( [ 'sass_watcher', 'js_watcher', 'langpack_watcher' ] ) );
 
-let js_watcher = gulp.task('js_watcher'),
-	watch = gulp.task('watch');
+const js_watcher = gulp.task( 'js_watcher' ),
+	watch = gulp.task( 'watch' );
 
-js_watcher.description = "Starts all js file watchers.";
-watch.description = "Start the file watchers.";
-
+js_watcher.description = 'Starts all js file watchers.';
+watch.description = 'Start the file watchers.';
