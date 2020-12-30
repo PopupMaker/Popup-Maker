7900f6a95c0f6204a76cbaf27f2029e5b2a0ca8b
Daniel Iser <daniel@code-atlantic.com>
2020-12-30 15:56:09 -0500

    Update method signature

--- classes/Utils/Blocks.php
+++ classes/Utils/Blocks.php
@@ -20,11 +20,11 @@ class PUM_Utils_Blocks {
 	 * Find blocks with matching name in given list of blocks.
 	 *
 	 * @param array  $blocks Array of blocks.
-	 * @param string $searchName Block name to look for.
+	 * @param string $search Block name to look for. Defaults to finding all blocks with pum/.* prefix. Accepts regex string.
 	 *
 	 * @return array
 	 */
-	public static function find_blocks( $blocks, $searchName = 'pum/*' ) {
+	public static function find_blocks( $blocks, $block_name = 'pum\/.*' ) {
 		$foundBlocks = [];
 
 		foreach ( $blocks as $block ) {
@@ -32,7 +32,7 @@ class PUM_Utils_Blocks {
 				$foundBlocks = array_merge( $foundBlocks, self::find_blocks( $block['innerBlocks'] ) );
 			}
 
-			if ( $searchName === $block['blockName'] ) {
+			if ( $block_name === $block['blockName'] ) {
 				$foundBlocks[] = $block;
 			}
 		}
