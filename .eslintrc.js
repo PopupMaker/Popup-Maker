ae7b5a4dfdc97c9ea7fedc43f2372633ae7c1fb3
Daniel Iser <daniel@code-atlantic.com>
2020-12-30 19:13:12 -0500

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,49 @@
 const eslintConfig = {
 	root: true,
 	extends: [
-		"eslint:recommended",
-		"plugin:@wordpress/eslint-plugin/recommended-with-formatting",
-		"plugin:@wordpress/eslint-plugin/jsdoc",
-		"plugin:eslint-comments/recommended"
+		'eslint:recommended',
+		'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
+		'plugin:@wordpress/eslint-plugin/jsdoc',
+		'plugin:eslint-comments/recommended',
 	],
-	plugins: ["standard", "import", "promise"],
+	plugins: [ 'standard', 'import', 'promise' ],
 	globals: {
-		wp: "readonly",
-		pum_vars: "readonly",
-		pum_site_vars: "readonly",
-		pum_admin_vars: "readonly"
+		_: 'readonly',
+		wp: 'readonly',
+		pum_vars: 'readonly',
+		pum_site_vars: 'readonly',
+		pum_admin_vars: 'readonly',
+		pum_block_editor_vars: 'readonly',
 	},
 	env: {
 		browser: true,
-		jquery: true
+		jquery: true,
 	},
 	settings: {
 		jsdoc: {
-			mode: "typescript"
-		}
+			mode: 'typescript',
+		},
 	},
 	rules: {},
 	overrides: [
 		{
 			// Turns off some of esnext rules for our assets JS until we migrate to babel or other.
-			files: ["assets/js/**/*.js"],
+			files: [ 'assets/js/**/*.js' ],
 			rules: {
-				"arrow-parens": "off",
-				"arrow-spacing": "off",
-				"computed-property-spacing": "off",
-				"constructor-super": "off",
-				"no-const-assign": "off",
-				"no-dupe-class-members": "off",
-				"no-duplicate-imports": "off",
-				"no-useless-computed-key": "off",
-				"no-useless-constructor": "off",
-				"no-var": "off",
-				"object-shorthand": "off"
-			}
-		}
-	]
+				'arrow-parens': 'off',
+				'arrow-spacing': 'off',
+				'computed-property-spacing': 'off',
+				'constructor-super': 'off',
+				'no-const-assign': 'off',
+				'no-dupe-class-members': 'off',
+				'no-duplicate-imports': 'off',
+				'no-useless-computed-key': 'off',
+				'no-useless-constructor': 'off',
+				'no-var': 'off',
+				'object-shorthand': 'off',
+			},
+		},
+	],
 };
 
 module.exports = eslintConfig;
