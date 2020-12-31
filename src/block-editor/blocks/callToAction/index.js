ec7c1ab1ad654de428bb58511d21ee5ce1e5dbb4
Daniel Iser <daniel@code-atlantic.com>
2020-12-30 19:11:48 -0500

    Finally a working cta button block starting point
    Setup entry for block styles in webpack

--- src/block-editor/blocks/callToAction/index.js
+++ src/block-editor/blocks/callToAction/index.js
@@ -1,46 +1,199 @@
 /**
+ * NPM dependencies
+ */
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet( '1234567890abcdef', 10 );
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
-import { withSpokenMessages } from '@wordpress/components';
+import { addFilter, applyFilters } from '@wordpress/hooks';
+
+/**
+ * Popup Maker Dependencies
+ */
+import LogoIcon from '../../icons/logo';
 
 /**
  * Internal dependencies
  */
-import LogoIcon from '../../icons/logo';
+import edit from './edit';
+import metadata from './block.json';
+import save from './save';
 
-const title = __( 'Popup Call to Action', 'popup-maker' );
-const description = __(
-	'Insert a call to action to let users convert to a specific action.',
-	'popup-maker'
-);
+const { debounce } = _;
 
-export const name = `popup-maker/call-to-action`;
+const { name, category, parent, attributes, supports } = metadata;
+
+export { metadata, name };
+
 export const settings = {
-	title,
-	description,
-	category: 'common',
+	name,
+	title: __( 'CTA Button', 'popup-maker' ),
+	description: __(
+		'Insert a call to action to let users convert to a specific action.',
+		'popup-maker'
+	),
+	category,
+	//  parent,
 	icon: LogoIcon,
-	keywords: [
+	attributes,
+	supports,
+	keywords: applyFilters( 'pum/cta-block/keywords', [
+		__( 'link' ),
 		__( 'cta', 'popup-maker' ),
 		__( 'button', 'popup-maker' ),
 		__( 'call to action', 'popup-maker' ),
-	],
-	styles: [
-		{
-			name: 'button-link',
-			label: __( 'Button', 'popup-maker' ),
-			isDefault: true,
-		},
-		{
-			name: 'text-link',
-			label: __( 'Text Link', 'popup-maker' ),
-		},
-	],
-	attributes: {
-		cta_type: {
-			type: 'string',
+	] ),
+	example: {
+		attributes: {
+			className: 'is-style-fill',
+			backgroundColor: '#8eae1b',
+			textColor: '#ffffff',
+			text: __( 'Call to Action', 'popup-maker' ),
 		},
 	},
+	styles: [
+		{ name: 'fill', label: __( 'Fill' ), isDefault: true },
+		{ name: 'outline', label: __( 'Outline' ) },
+		{
+			name: 'text-only',
+			label: __( 'Text Only', 'popup-maker' ),
+			textColor: '#333333',
+		},
+	],
+	// variations: applyFilters( 'pum/cta-block/variations', [
+	// 	{
+	// 		name: 'blue',
+	// 		title: __( 'Blue Quote' ),
+	// 		isDefault: true,
+	// 		attributes: { className: 'is-style-blue-quote' },
+	// 	},
+	// ] ),
+	edit,
+	save,
+	merge: ( a, { text = '' } ) => ( {
+		...a,
+		text: ( a.text || '' ) + text,
+	} ),
 };
+
+/**
+ * Set UUID for any CTA block that is missing it.
+ *
+ * @param blocks
+ */
+// addFilter(
+// 	'blocks.getBlockAttributes',
+// 	'pum/ctas',
+// 	( blockAttributes, blockType, innerHTML, attributes ) => {
+// 		if ( name === blockType.name ) {
+// 			if ( blockAttributes.pid === undefined || ! blockAttributes.pid ) {
+// 				blockAttributes.pid = wp.data
+// 					.select( 'core/editor' )
+// 					.getCurrentPostId();
+// 			}
+
+// 			if (
+// 				blockAttributes.uuid === undefined ||
+// 				! blockAttributes.uuid
+// 			) {
+// 				blockAttributes.uuid = nanoid();
+// 			}
+// 			console.log( {
+// 				blockAttributes,
+// 				blockType,
+// 				innerHTML,
+// 				attributes,
+// 			} );
+// 		}
+// 		return blockAttributes;
+// 	}
+// );
+
+/**
+ * Index of CTA blocks & UUIDs.
+ */
+const ctaBlocks = {
+	uuidByClientId: {},
+	clientIdByUuid: {},
+};
+
+/**
+ * Process blocks looking for CTAs to validate.
+ *
+ * @param {Object|Array} blocks List of blocks.
+ */
+const processBlocks = ( blocks ) => {
+	blocks.forEach( ( block ) => {
+		if ( [ 'core/column', 'core/columns' ].indexOf( block.name ) >= 0 ) {
+			processBlocks( block.innerBlocks );
+		} else if ( name === block.name ) {
+			let uuid = block.attributes.uuid || false;
+			let newUuid = false;
+
+			// if has no uuid, generate it.
+			if ( ! uuid ) {
+				uuid = newUuid = nanoid();
+			}
+
+			// if uuid is duplicate, replace it on most recent* block.
+			if (
+				undefined !== ctaBlocks.clientIdByUuid[ uuid ] &&
+				block.clientId !== ctaBlocks.clientIdByUuid[ uuid ]
+			) {
+				uuid = newUuid = nanoid();
+				ctaBlocks.uuidByClientId[ block.clientId ] = uuid;
+				ctaBlocks.clientIdByUuid[ uuid ] = block.clientId;
+			}
+
+			ctaBlocks.uuidByClientId[ block.clientId ] = uuid;
+			ctaBlocks.clientIdByUuid[ uuid ] = block.clientId;
+
+			// if uuid is duplicate, replace it on most recent* block.
+			if (
+				block.clientId !== ctaBlocks.clientIdByUuid[ uuid ] ||
+				uuid !== ctaBlocks.uuidByClientId[ block.clientId ]
+			) {
+				uuid = newUuid = nanoid();
+				ctaBlocks.uuidByClientId[ block.clientId ] = uuid;
+				ctaBlocks.clientIdByUuid[ uuid ] = block.clientId;
+			}
+
+			// If new uuid was generated, update the block.
+			if ( newUuid ) {
+				// Update attributes of another block
+				// wp.data.dispatch( 'core/editor' ).updateBlockAttributes( clientID, attributes )
+				wp.data
+					.dispatch( 'core/editor' )
+					.updateBlockAttributes( block.clientId, {
+						uuid: newUuid,
+					} );
+			}
+		}
+	} );
+};
+
+setTimeout( () => {
+	let blockState = wp.data.select( 'core/block-editor' ).getBlocks();
+
+	// Process blocks initially.
+	processBlocks( blockState );
+
+	wp.data.subscribe(
+		debounce( () => {
+			const newBlocksState = wp.data
+				.select( 'core/block-editor' )
+				.getBlocks();
+			if (
+				blockState.length !== newBlocksState.length ||
+				blockState !== newBlocksState
+			) {
+				processBlocks( newBlocksState );
+			}
+			// Update reference.
+			blockState = newBlocksState;
+		}, 300 )
+	);
+}, 1000 );
---
+++ src/block-editor/blocks/callToAction/save.js
@@ -0,0 +1,77 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet( '1234567890abcdef', 10 );
+
+/**
+ * WordPress dependencies
+ */
+import { RichText, useBlockProps } from '@wordpress/block-editor';
+import { useEffect } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import getColorAndStyleProps from './color-props';
+
+export default function save( { attributes } ) {
+	const {
+		borderRadius,
+		linkTarget,
+		rel,
+		text,
+		title,
+		url,
+		uuid,
+		pid,
+	} = attributes;
+	const colorProps = getColorAndStyleProps( attributes );
+	const buttonClasses = classnames(
+		'pum-cta-button__link',
+		colorProps.className,
+		{
+			'no-border-radius': borderRadius === 0,
+		},
+		'custom-class'
+	);
+	const buttonStyle = {
+		borderRadius: borderRadius ? borderRadius + 'px' : undefined,
+		...colorProps.style,
+	};
+
+	const urlParams = {
+		pid: pid || wp.data.select( 'core/editor' ).getCurrentPostId(),
+		cta: uuid,
+	};
+
+	const queryString = Object.keys( urlParams )
+		.map( ( key ) => {
+			return (
+				encodeURIComponent( key ) +
+				'=' +
+				encodeURIComponent( urlParams[ key ] )
+			);
+		} )
+		.join( '&' );
+
+	// The use of a `title` attribute here is soft-deprecated, but still applied
+	// if it had already been assigned, for the sake of backward-compatibility.
+	// A title will no longer be assigned for new or updated button block links.
+
+	return (
+		<div { ...useBlockProps.save() }>
+			<RichText.Content
+				tagName="a"
+				className={ buttonClasses }
+				href={ `?${ queryString }` }
+				title={ title }
+				style={ buttonStyle }
+				value={ text }
+				target={ linkTarget }
+				rel={ rel }
+			/>
+		</div>
+	);
+}
