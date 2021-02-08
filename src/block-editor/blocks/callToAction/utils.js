920537729832012cb8b352a599f9b255aa154ad1
Daniel Iser <daniel@code-atlantic.com>
2021-02-08 06:27:54 -0500

    Add new utility for getting field defaults

--- src/block-editor/blocks/callToAction/utils.js
+++ src/block-editor/blocks/callToAction/utils.js
@@ -1,7 +1,24 @@
 const { ctas } = pum_block_editor_vars;
-const getCta = ( key ) =>
-	callToActions[ key ] || callToActions[ Object.keys( callToActions )[ 0 ] ];
+const getCta = ( key ) => ctas[ key ] || ctas[ Object.keys( ctas )[ 0 ] ];
 
 const callToActions = Object.values( ctas );
 
-export { callToActions, getCta };
+const getDefaults = ( key ) => {
+	const cta = getCta( key );
+	const tabs = cta.fields;
+	const fields = {};
+
+	Object.keys( tabs ).forEach( ( tabId ) => {
+		Object.keys( tabs[ tabId ] ).forEach( ( fieldId ) => {
+			const std =
+				tabs[ tabId ][ fieldId ].std !== undefined
+					? tabs[ tabId ][ fieldId ].std
+					: null;
+			fields[ fieldId ] = std;
+		} );
+	} );
+
+	return fields;
+};
+
+export { callToActions, getCta, getDefaults };
