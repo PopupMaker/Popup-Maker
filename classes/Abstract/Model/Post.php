a696d33fccbb0a14d5a46a7aac94c3e773cee518
“Sarai” <Sarai.ace6@gmail.com>
2022-10-25 00:06:34 -0400

    Add param type in comment

--- classes/Abstract/Model/Post.php
+++ classes/Abstract/Model/Post.php
@@ -333,7 +333,7 @@ abstract class PUM_Abstract_Model_Post {
 	/**
 	 * Function get_meta.
 	 *
-	 * @param      $key
+	 * @param mixed  $key
 	 * @param bool $single  If true, return only the first value of the specified value.
 	 *
 	 * @return mixed|false
