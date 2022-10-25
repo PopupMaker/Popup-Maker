8b588470c57dd318a856c43d8113500f34b00609
“Sarai” <Sarai.ace6@gmail.com>
2022-10-25 00:43:26 -0400

    Fix loose comparison

--- classes/Abstract/Model/Post.php
+++ classes/Abstract/Model/Post.php
@@ -429,7 +429,7 @@ abstract class PUM_Abstract_Model_Post {
 	 * @return bool
 	 */
 	public function is_published() {
-		return get_post_status( $this->ID ) == 'publish';
+		return get_post_status( $this->ID ) === 'publish';
 	}
 
 	/**
