aa4f4de94dd0d4b4088f654148e0f4d8872b2b93
“Sarai” <Sarai.ace6@gmail.com>
2022-11-08 06:24:08 -0500

    Remove blank lines after the class comment

--- classes/Integration/Builder/KingComposer.php
+++ classes/Integration/Builder/KingComposer.php
@@ -5,7 +5,6 @@
  * @package     PUM
  * @copyright   Copyright (c) 2022, Code Atlantic LLC
  */
-
 class PUM_Integration_Builder_KingComposer extends PUM_Abstract_Integration {
 
 	/**
--- classes/Integration/Builder/VisualComposer.php
+++ classes/Integration/Builder/VisualComposer.php
@@ -5,7 +5,6 @@
  * @package     PUM
  * @copyright   Copyright (c) 2022, Code Atlantic LLC
  */
-
 class PUM_Integration_Builder_VisualComposer extends PUM_Abstract_Integration {
 
 	/**
