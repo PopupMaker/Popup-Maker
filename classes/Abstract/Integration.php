adc9fd146aba135943ce10425d914c2ceb990413
“Sarai” <Sarai.ace6@gmail.com>
2022-11-07 19:43:51 -0500

    Add missing short description in doc comment

--- classes/Abstract/Integration.php
+++ classes/Abstract/Integration.php
@@ -12,21 +12,29 @@
 abstract class PUM_Abstract_Integration implements PUM_Interface_Integration {
 
 	/**
+	 * $key variable
+	 *
 	 * @var string
 	 */
 	public $key;
 
 	/**
+	 * $type variable
+	 *
 	 * @var string
 	 */
 	public $type;
 
 	/**
+	 * Label function for Integration Abstract.
+	 *
 	 * @return string
 	 */
 	abstract public function label();
 
 	/**
+	 * Enabled function for Integration Abstract.
+	 *
 	 * @return bool
 	 */
 	abstract public function enabled();
