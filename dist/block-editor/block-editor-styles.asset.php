2b50b28a5ea4a22623b0c065015b8b42697d293b
“Sarai” <Sarai.ace6@gmail.com>
2022-11-09 22:56:54 -0500

    Add missing file doc comment

--- dist/block-editor/block-editor-styles.asset.php
+++ dist/block-editor/block-editor-styles.asset.php
@@ -1,4 +1,12 @@
-<?php return [
+<?php
+/**
+ * Block editor styles asset
+ *
+ * @package     PUM
+ * @copyright   Copyright (c) 2022, Code Atlantic LLC
+ */
+
+return [
 	'dependencies' => [ 'wp-polyfill' ],
 	'version'      => '6008e2fb9b65943c753ca6af5add6346',
 ];
--- dist/block-editor/block-editor.asset.php
+++ dist/block-editor/block-editor.asset.php
@@ -1,4 +1,12 @@
-<?php return [
+<?php
+/**
+ * Block editor asset
+ *
+ * @package     PUM
+ * @copyright   Copyright (c) 2022, Code Atlantic LLC
+ */
+
+return [
 	'dependencies' => [ 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-rich-text' ],
 	'version'      => 'e9b2139f4d815a1ed49867b4a3fef47d',
 ];
