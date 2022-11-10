aadbfecf6fd36a8fc8594f16c71c594eb5695cdb
“Sarai” <Sarai.ace6@gmail.com>
2022-11-09 21:08:55 -0500

    Change comment to use "/**"  for a class comment

--- classes/Popup.php
+++ classes/Popup.php
@@ -6,5 +6,5 @@
  * @copyright   Copyright (c) 2022, Code Atlantic LLC
  */
 
-// Empty class extender here for backward compatibility.
+/** Empty class extender here for backward compatibility. */
 class PUM_Popup extends PUM_Model_Popup {}
