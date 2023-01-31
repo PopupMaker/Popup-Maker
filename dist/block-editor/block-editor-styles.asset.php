9044a414223712ca4b306228ba33c102e12a679a
Daniel L. Iser <daniel@code-atlantic.com>
2023-01-31 04:46:19 -0500

    Remove comment from generated file

--- dist/block-editor/block-editor-styles.asset.php
+++ dist/block-editor/block-editor-styles.asset.php
@@ -1,9 +1,1 @@
-<?php
-/**
- * Block editor styles asset
- *
- * @package   PUM
- * @copyright Copyright (c) 2023, Code Atlantic LLC
- */
-
-return array('dependencies' => array('wp-polyfill'), 'version' => '6008e2fb9b65943c753ca6af5add6346');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '6008e2fb9b65943c753ca6af5add6346');
