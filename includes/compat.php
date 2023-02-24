40a66c602d214d6e68fa0ec1233f006a01f23853
Daniel Iser <daniel@code-atlantic.com>
2023-02-24 04:16:56 -0500

    Add backcompat fix for WP >5.3

--- includes/compat.php
+++ includes/compat.php
@@ -261,3 +261,100 @@ if ( ! function_exists( 'has_blocks' ) ) {
 		return false !== strpos( (string) $post, '<!-- wp:' );
 	}
 }
+
+/**
+ * Aweful hack for backward compatibility with WP <5.3.
+ */
+if ( ! function_exists( 'wp_date' ) ) {
+	function wp_date( $format, $timestamp = null, $timezone = null ) {
+		global $wp_locale;
+
+		if ( null === $timestamp ) {
+			$timestamp = time();
+		} elseif ( ! is_numeric( $timestamp ) ) {
+			return false;
+		}
+
+		if ( ! $timezone ) {
+			if ( function_exists( 'wp_timezone' ) ) {
+				$timezone = wp_timezone();
+			} else {
+				// Get the server timezone
+				$server_timezone = get_option( 'timezone_string' );
+
+				// If timezone_string is not set, get the default timezone of the server
+				if ( empty( $server_timezone ) ) {
+					$server_timezone = date_default_timezone_get();
+				}
+
+				$timezone = new DateTimeZone( $server_timezone );
+			}
+		}
+
+		$datetime = date_create( '@' . $timestamp );
+		$datetime->setTimezone( $timezone );
+
+		if ( empty( $wp_locale->month ) || empty( $wp_locale->weekday ) ) {
+			$date = $datetime->format( $format );
+		} else {
+			// We need to unpack shorthand `r` format because it has parts that might be localized.
+			$format = preg_replace( '/(?<!\\\\)r/', DATE_RFC2822, $format );
+
+			$new_format    = '';
+			$format_length = strlen( $format );
+			$month         = $wp_locale->get_month( $datetime->format( 'm' ) );
+			$weekday       = $wp_locale->get_weekday( $datetime->format( 'w' ) );
+
+			for ( $i = 0; $i < $format_length; $i++ ) {
+				switch ( $format[ $i ] ) {
+					case 'D':
+						$new_format .= addcslashes( $wp_locale->get_weekday_abbrev( $weekday ), '\\A..Za..z' );
+						break;
+					case 'F':
+						$new_format .= addcslashes( $month, '\\A..Za..z' );
+						break;
+					case 'l':
+						$new_format .= addcslashes( $weekday, '\\A..Za..z' );
+						break;
+					case 'M':
+						$new_format .= addcslashes( $wp_locale->get_month_abbrev( $month ), '\\A..Za..z' );
+						break;
+					case 'a':
+						$new_format .= addcslashes( $wp_locale->get_meridiem( $datetime->format( 'a' ) ), '\\A..Za..z' );
+						break;
+					case 'A':
+						$new_format .= addcslashes( $wp_locale->get_meridiem( $datetime->format( 'A' ) ), '\\A..Za..z' );
+						break;
+					case '\\':
+						$new_format .= $format[ $i ];
+
+						// If character follows a slash, we add it without translating.
+						if ( $i < $format_length ) {
+							$new_format .= $format[ ++$i ];
+						}
+						break;
+					default:
+						$new_format .= $format[ $i ];
+						break;
+				}
+			}
+
+			$date = $datetime->format( $new_format );
+			$date = wp_maybe_decline_date( $date, $format );
+		}
+
+		/**
+		 * Filters the date formatted based on the locale.
+		 *
+		 * @since 5.3.0
+		 *
+		 * @param string       $date      Formatted date string.
+		 * @param string       $format    Format to display the date.
+		 * @param int          $timestamp Unix timestamp.
+		 * @param DateTimeZone $timezone  Timezone.
+		 */
+		$date = apply_filters( 'wp_date', $date, $format, $timestamp, $timezone );
+
+		return $date;
+	}
+}
