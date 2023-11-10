a7d338e9882b8a5ac1009e574159cf7050382bba
Sarai Acevedo <sarai@code-atlantic.com>
2023-11-10 13:27:36 -0500

    Add check for plugin_array

--- classes/Admin/Shortcode/UI.php
+++ classes/Admin/Shortcode/UI.php
@@ -167,12 +167,15 @@ class PUM_Admin_Shortcode_UI {
 	 * @return array
 	 */
 	public static function mce_external_plugins( $plugin_array ) {
-		return array_merge(
-			$plugin_array,
-			[
-				'pum_shortcodes' => add_query_arg( [ 'version' => Popup_Maker::$VER ], PUM_Admin_Assets::$js_url . 'mce-buttons' . PUM_Admin_Assets::$suffix . '.js' ),
-			]
-		);
+
+		if( is_array( $plugin_array ) ){
+			return array_merge(
+				$plugin_array,
+				[
+					'pum_shortcodes' => add_query_arg( [ 'version' => Popup_Maker::$VER ], PUM_Admin_Assets::$js_url . 'mce-buttons' . PUM_Admin_Assets::$suffix . '.js' ),
+				]
+			);
+		} else return;
 	}
 
 	public static function do_shortcode() {
