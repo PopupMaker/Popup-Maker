31545b7e8b147ebf317b665d636311b4a9a316c2
Daniel Iser <daniel@code-atlantic.com>
2024-08-14 03:21:17 -0400

    Limit when extension updates can be checked
    Only handle for cron or privelidged users.

--- classes/Extension/License.php
+++ classes/Extension/License.php
@@ -174,9 +174,16 @@ class PUM_Extension_License {
 	 * @return  void
 	 */
 	public function auto_updater() {
+		// To support auto-updates, this needs to run during the wp_version_check cron job for privileged users.
+		$doing_cron = defined( 'DOING_CRON' ) && DOING_CRON;
+		if ( ! current_user_can( 'manage_options' ) && ! $doing_cron ) {
+			return;
+		}
+
 		$args = [
 			'version' => $this->version,
 			'license' => $this->license,
+			'item_id' => $this->item_id,
 			'author'  => $this->author,
 			'beta'    => PUM_Admin_Tools::extension_has_beta_support( $this->item_shortname ),
 		];
