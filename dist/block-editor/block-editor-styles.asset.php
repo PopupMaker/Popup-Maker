54df2976e2d798ec40c0752a4d203417e2348f06
Daniel Iser <daniel@code-atlantic.com>
2024-08-18 04:40:08 -0400

    Update build assets

--- dist/block-editor/block-editor-styles.asset.php
+++ dist/block-editor/block-editor-styles.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '6008e2fb9b65943c753ca6af5add6346');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '9499686f1c5247706ca796b0d7d44e5d');
