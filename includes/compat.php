c2bfb7f86df8f213a5a5e3a34957fa5d90f6fdaf
Daniel Iser <daniel@code-atlantic.com>
2024-08-23 02:11:35 -0400

    Remove function used only for PHP 5.2

--- includes/compat.php
+++ includes/compat.php
@@ -83,59 +83,6 @@ if ( ! function_exists( 'current_action' ) ) {
 	}
 }
 
-if ( ! function_exists( 'get_called_class' ) ) {
-	function get_called_class( $bt = false, $l = 1 ) {
-		if ( ! $bt ) {
-			// phpcs:ignore PHPCompatibility.FunctionUse.ArgumentFunctionsReportCurrentValue.NeedsInspection, WordPress.PHP.DevelopmentFunctions.error_log_debug_backtrace
-			$bt = debug_backtrace();
-		}
-		if ( ! isset( $bt[ $l ] ) ) {
-			throw new Exception( 'Cannot find called class -> stack level too deep.' );
-		}
-		if ( ! isset( $bt[ $l ]['type'] ) ) {
-			throw new Exception( 'type not set' );
-		} else {
-			switch ( $bt[ $l ]['type'] ) {
-				case '::':
-					$lines       = file( $bt[ $l ]['file'] );
-					$i           = 0;
-					$caller_line = '';
-					do {
-						++$i;
-						$caller_line = $lines[ $bt[ $l ]['line'] - $i ] . $caller_line;
-					} while ( stripos( $caller_line, $bt[ $l ]['function'] ) === false );
-					preg_match( '/([a-zA-Z0-9\_]+)::' . $bt[ $l ]['function'] . '/', $caller_line, $matches );
-					if ( ! isset( $matches[1] ) ) {
-						// must be an edge case.
-						throw new Exception( 'Could not find caller class: originating method call is obscured.' );
-					}
-					switch ( $matches[1] ) {
-						case 'self':
-						case 'parent':
-							return get_called_class( $bt, $l + 1 );
-						default:
-							return $matches[1];
-					}
-					// won't get here.
-				case '->':
-					switch ( $bt[ $l ]['function'] ) {
-						case '__get':
-							// edge case -> get class of calling object
-							if ( ! is_object( $bt[ $l ]['object'] ) ) {
-								throw new Exception( 'Edge case fail. __get called on non object.' );
-							}
-
-							return get_class( $bt[ $l ]['object'] );
-						default:
-							return get_class( $bt[ $l ]['object'] );
-					}
-				default:
-					throw new Exception( 'Unknown backtrace method type' );
-			}
-		}
-	}
-}
-
 if ( ! function_exists( 'get_term_name' ) ) {
 	function get_term_name( $term_id, $taxonomy ) {
 		$term = get_term_by( 'id', absint( $term_id ), $taxonomy );
