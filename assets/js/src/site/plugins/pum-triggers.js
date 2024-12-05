2b91c8f7c6856b9217d8ee307b18edc1c7e6dbd4
Daniel Iser <daniel@code-atlantic.com>
2024-12-05 04:19:57 -0500

    Update trigger registration to allow hook

--- assets/js/src/site/plugins/pum-triggers.js
+++ assets/js/src/site/plugins/pum-triggers.js
@@ -11,6 +11,11 @@
 
     $.extend($.fn.popmake.methods, {
         addTrigger: function (type) {
+			// Init Triggers.
+			getTriggers();
+
+			// TODO Refactor to use getTriggers return for method list.
+
             // Method calling logic
             if ($.fn.popmake.triggers[type]) {
                 return $.fn.popmake.triggers[type].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -22,136 +27,19 @@
         }
     });
 
+	let popupTriggers = {};
+	function getTriggers() {
+		if ( Object.keys( popupTriggers ).length ) {
+			return popupTriggers;
+		}
 
-	$.fn.popmake.triggers = window.PUM.hooks.applyFilters(
-		'popupMaker.triggers',
-		{
-			auto_open: function (settings) {
-				var $popup = PUM.getPopup(this);
+		popupTriggers = $.fn.popmake.triggers = window.PUM.hooks.applyFilters(
+			'popupMaker.triggers',
+			{
+				auto_open: function (settings) {
+					var $popup = PUM.getPopup(this);
 
-				// Set a delayed open.
-				setTimeout(function () {
-					// If the popup is already open return.
-					if ($popup.popmake('state', 'isOpen')) {
-						return;
-					}
-
-					// If cookie exists or conditions fail return.
-					if (
-						$popup.popmake('checkCookies', settings) ||
-						!$popup.popmake('checkConditions')
-					) {
-						return;
-					}
-
-					// Set the global last open trigger to the a text description of the trigger.
-					$.fn.popmake.last_open_trigger =
-						'Auto Open - Delay: ' + settings.delay;
-
-					// Open the popup.
-					$popup.popmake('open');
-				}, settings.delay);
-			},
-			click_open: function (settings) {
-				var $popup = PUM.getPopup(this),
-					popup_settings = $popup.popmake('getSettings'),
-					trigger_selectors = [
-						'.popmake-' + popup_settings.id,
-						'.popmake-' + decodeURIComponent(popup_settings.slug),
-						'a[href$="#popmake-' + popup_settings.id + '"]',
-					],
-					trigger_selector;
-
-				if (
-					settings.extra_selectors &&
-					settings.extra_selectors !== ''
-				) {
-					trigger_selectors.push(settings.extra_selectors);
-				}
-
-				trigger_selectors = pum.hooks.applyFilters(
-					'pum.trigger.click_open.selectors',
-					trigger_selectors,
-					settings,
-					$popup
-				);
-
-				trigger_selector = trigger_selectors.join(', ');
-
-				$(trigger_selector)
-					.addClass('pum-trigger')
-					.css({ cursor: 'pointer' });
-
-				$(document).on(
-					'click.pumTrigger',
-					trigger_selector,
-					function (event) {
-						var $trigger = $(this),
-							do_default = settings.do_default || false;
-
-						// If trigger is inside of the popup that it opens, do nothing.
-						if ($popup.has($trigger).length > 0) {
-							return;
-						}
-
-						// If the popup is already open return.
-						if ($popup.popmake('state', 'isOpen')) {
-							return;
-						}
-
-						// If cookie exists or conditions fail return.
-						if (
-							$popup.popmake('checkCookies', settings) ||
-							!$popup.popmake('checkConditions')
-						) {
-							return;
-						}
-
-						if ($trigger.data('do-default')) {
-							do_default = $trigger.data('do-default');
-						} else if (
-							$trigger.hasClass('do-default') ||
-							$trigger.hasClass('popmake-do-default') ||
-							$trigger.hasClass('pum-do-default')
-						) {
-							do_default = true;
-						}
-
-						// If trigger has the class do-default we don't prevent default actions.
-						if (
-							!event.ctrlKey &&
-							!pum.hooks.applyFilters(
-								'pum.trigger.click_open.do_default',
-								do_default,
-								$popup,
-								$trigger
-							)
-						) {
-							event.preventDefault();
-							event.stopPropagation();
-						}
-
-						// Set the global last open trigger to the clicked element.
-						$.fn.popmake.last_open_trigger = $trigger;
-
-						// Open the popup.
-						$popup.popmake('open');
-					}
-				);
-			},
-			form_submission: function (settings) {
-				var $popup = PUM.getPopup(this);
-
-				settings = $.extend(
-					{
-						form: '',
-						formInstanceId: '',
-						delay: 0,
-					},
-					settings
-				);
-
-				var onSuccess = function () {
+					// Set a delayed open.
 					setTimeout(function () {
 						// If the popup is already open return.
 						if ($popup.popmake('state', 'isOpen')) {
@@ -167,38 +55,163 @@
 						}
 
 						// Set the global last open trigger to the a text description of the trigger.
-						$.fn.popmake.last_open_trigger = 'Form Submission';
+						$.fn.popmake.last_open_trigger =
+							'Auto Open - Delay: ' + settings.delay;
 
 						// Open the popup.
 						$popup.popmake('open');
 					}, settings.delay);
-				};
+				},
+				click_open: function (settings) {
+					var $popup = PUM.getPopup(this),
+						popup_settings = $popup.popmake('getSettings'),
+						trigger_selectors = [
+							'.popmake-' + popup_settings.id,
+							'.popmake-' + decodeURIComponent(popup_settings.slug),
+							'a[href$="#popmake-' + popup_settings.id + '"]',
+						],
+						trigger_selector;
 
-				// Listen for integrated form submissions.
-				PUM.hooks.addAction(
-					'pum.integration.form.success',
-					function (form, args) {
-						if (!settings.form.length) {
-							return;
-						}
-
-						if (
-							PUM.integrations.checkFormKeyMatches(
-								settings.form,
-								settings.formInstanceId,
-								args
-							)
-						) {
-							onSuccess();
-						}
+					if (
+						settings.extra_selectors &&
+						settings.extra_selectors !== ''
+					) {
+						trigger_selectors.push(settings.extra_selectors);
 					}
-				);
-			},
-			admin_debug: function () {
-				PUM.getPopup(this).popmake('open');
-			},
-		}
-	);
+
+					trigger_selectors = pum.hooks.applyFilters(
+						'pum.trigger.click_open.selectors',
+						trigger_selectors,
+						settings,
+						$popup
+					);
+
+					trigger_selector = trigger_selectors.join(', ');
+
+					$(trigger_selector)
+						.addClass('pum-trigger')
+						.css({ cursor: 'pointer' });
+
+					$(document).on(
+						'click.pumTrigger',
+						trigger_selector,
+						function (event) {
+							var $trigger = $(this),
+								do_default = settings.do_default || false;
+
+							// If trigger is inside of the popup that it opens, do nothing.
+							if ($popup.has($trigger).length > 0) {
+								return;
+							}
+
+							// If the popup is already open return.
+							if ($popup.popmake('state', 'isOpen')) {
+								return;
+							}
+
+							// If cookie exists or conditions fail return.
+							if (
+								$popup.popmake('checkCookies', settings) ||
+								!$popup.popmake('checkConditions')
+							) {
+								return;
+							}
+
+							if ($trigger.data('do-default')) {
+								do_default = $trigger.data('do-default');
+							} else if (
+								$trigger.hasClass('do-default') ||
+								$trigger.hasClass('popmake-do-default') ||
+								$trigger.hasClass('pum-do-default')
+							) {
+								do_default = true;
+							}
+
+							// If trigger has the class do-default we don't prevent default actions.
+							if (
+								!event.ctrlKey &&
+								!pum.hooks.applyFilters(
+									'pum.trigger.click_open.do_default',
+									do_default,
+									$popup,
+									$trigger
+								)
+							) {
+								event.preventDefault();
+								event.stopPropagation();
+							}
+
+							// Set the global last open trigger to the clicked element.
+							$.fn.popmake.last_open_trigger = $trigger;
+
+							// Open the popup.
+							$popup.popmake('open');
+						}
+					);
+				},
+				form_submission: function (settings) {
+					var $popup = PUM.getPopup(this);
+
+					settings = $.extend(
+						{
+							form: '',
+							formInstanceId: '',
+							delay: 0,
+						},
+						settings
+					);
+
+					var onSuccess = function () {
+						setTimeout(function () {
+							// If the popup is already open return.
+							if ($popup.popmake('state', 'isOpen')) {
+								return;
+							}
+
+							// If cookie exists or conditions fail return.
+							if (
+								$popup.popmake('checkCookies', settings) ||
+								!$popup.popmake('checkConditions')
+							) {
+								return;
+							}
+
+							// Set the global last open trigger to the a text description of the trigger.
+							$.fn.popmake.last_open_trigger = 'Form Submission';
+
+							// Open the popup.
+							$popup.popmake('open');
+						}, settings.delay);
+					};
+
+					// Listen for integrated form submissions.
+					PUM.hooks.addAction(
+						'pum.integration.form.success',
+						function (form, args) {
+							if (!settings.form.length) {
+								return;
+							}
+
+							if (
+								PUM.integrations.checkFormKeyMatches(
+									settings.form,
+									settings.formInstanceId,
+									args
+								)
+							) {
+								onSuccess();
+							}
+						}
+					);
+				},
+				admin_debug: function () {
+					PUM.getPopup(this).popmake('open');
+				},
+			}
+		);
+
+		return popupTriggers;
+	};
 
     // Register All Triggers for a Popup
     $(document)
