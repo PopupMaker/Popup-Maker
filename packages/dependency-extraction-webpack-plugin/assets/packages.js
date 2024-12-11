2b668186f665798e7e38aedc6d66f17c14332813
Daniel Iser <daniel@code-atlantic.com>
2024-12-11 05:45:04 -0500

    Update assets

--- packages/dependency-extraction-webpack-plugin/assets/packages.js
+++ packages/dependency-extraction-webpack-plugin/assets/packages.js
@@ -1,6 +1,9 @@
 module.exports = [
 	'@popup-maker/admin-bar',
+	'@popup-maker/admin-marketing',
 	'@popup-maker/block-library',
+	'@popup-maker/block-editor',
+	'@popup-maker/components',
 	'@popup-maker/front-end',
 	'@popup-maker/dependency-extraction-webpack-plugin',
 ];
