5e94570d64ea6eece436f02fba0e0177fc1f4c25
Daniel Iser <daniel@code-atlantic.com>
2024-12-12 01:41:23 -0500

    Update package list

--- packages/dependency-extraction-webpack-plugin/assets/packages.js
+++ packages/dependency-extraction-webpack-plugin/assets/packages.js
@@ -1,9 +1,11 @@
 module.exports = [
 	'@popup-maker/admin-bar',
 	'@popup-maker/admin-marketing',
-	'@popup-maker/block-library',
 	'@popup-maker/block-editor',
+	'@popup-maker/block-library',
 	'@popup-maker/components',
-	'@popup-maker/front-end',
+	'@popup-maker/core-data',
+	'@popup-maker/custom-templated-path-webpack-plugin',
+	'@popup-maker/data',
 	'@popup-maker/dependency-extraction-webpack-plugin',
 ];
