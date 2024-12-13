2632a9bfc6d769dd1428217074cb8d0d4b84abc2
Daniel Iser <daniel@code-atlantic.com>
2024-12-13 05:38:42 -0500

    Comment out unneeded code

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,12 @@ const eslintConfig = {
 	env: {
 		jquery: true,
 	},
-	parserOptions: {
-		requireConfigFile: false,
-		babelOptions: {
-			presets: [ require.resolve( '@wordpress/babel-preset-default' ) ],
-		},
-	},
+	// parserOptions: {
+	// 	requireConfigFile: false,
+	// 	babelOptions: {
+	// 		presets: [ require.resolve( '@wordpress/babel-preset-default' ) ],
+	// 	},
+	// },
 	rules: {
 		'import/no-unresolved': [
 			'error',
