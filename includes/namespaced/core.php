351b2bb7b06da6ae5740170b5fda1f10ae9369b5
Daniel Iser <daniel@code-atlantic.com>
2024-12-22 06:34:01 -0500

    Added cta permission

--- includes/namespaced/core.php
+++ includes/namespaced/core.php
@@ -2,8 +2,6 @@
 /**
  * Core functions.
  *
- * @since X.X.X
- *
  * @package   PopupMaker
  * @copyright Copyright (c) 2024, Code Atlantic LLC
  */
@@ -16,10 +14,13 @@ defined( 'ABSPATH' ) || exit;
  * Returns an array of the default permissions.
  *
  * @return array<string,string> Default permissions.
+ *
+ * @since X.X.X
  */
 function get_default_permissions() {
 	return [
 		// Settings.
+		'edit_ctas'         => 'manage_options',
 		'edit_popups'       => 'manage_options',
 		'edit_popup_themes' => 'manage_options',
 		'manage_settings'   => 'manage_options',
