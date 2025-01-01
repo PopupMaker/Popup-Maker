9feda8a685582c34414765c68f00beaf9edb54f2
Daniel Iser <daniel@code-atlantic.com>
2025-01-01 02:00:49 -0500

    Clean up url handler

--- classes/Controllers/CallToActions.php
+++ classes/Controllers/CallToActions.php
@@ -32,9 +32,11 @@ class CallToActions extends Controller {
 	 * Redirects when needed.
 	 */
 	public function template_redirect() {
+		$cta_args = [ 'cta', 'pid' ];
+
 		/* phpcs:disable WordPress.Security.NonceVerification.Recommended */
 		$cta_uuid = ! empty( $_GET['cta'] ) ? sanitize_text_field( wp_unslash( $_GET['cta'] ) ) : '';
-		$popup_id = ! empty( $_GET['pid'] ) ? absint( $_GET['pid'] ) : 0;
+		$popup_id = ! empty( $_GET['pid'] ) ? absint( $_GET['pid'] ) : null;
 		/* phpcs:enable WordPress.Security.NonceVerification.Recommended */
 
 		if ( empty( $cta_uuid ) ) {
@@ -52,20 +54,19 @@ class CallToActions extends Controller {
 			return;
 		}
 
-		$popup = $popup_id > 0 ? pum_get_popup( $popup_id ) : false;
-
 		/**
-		 * Allow extensions to handle their own CTA types
+		 * Allow extensions to handle their own CTA types.
 		 *
-		 * @param bool $handled Whether the CTA was handled
+		 * @param bool  $handled Whether the CTA was handled.
+		 * @param CTA   $cta The CTA object.
 		 * @param array $args {
-		 *     @type int $popup_id The popup ID if any
-		 *     @type CTA $cta The CTA object
+		 *     @type int    $popup_id The popup ID if any
+		 *     @type string $cta_uuid The CTA UUID
 		 * }
 		 */
-		$actioned = apply_filters('popup_maker/cta_action', false, [
+		$actioned = apply_filters('popup_maker/cta_action', false, $cta, [
 			'popup_id' => $popup_id,
-			'cta'      => $cta,
+			'cta_uuid' => $cta_uuid,
 		]);
 
 		if ( ! $actioned ) {
@@ -76,21 +77,30 @@ class CallToActions extends Controller {
 						'cta_id' => $cta->id,
 					]);
 
-					$url = esc_url_raw( $cta->get_setting( 'url' ) );
+					// Get url from CTA (defaults to current URL) and strip CTA query args.
+					$url = remove_query_arg(
+						$cta_args,
+						$cta->get_setting( 'url' )
+					);
 
-					wp_safe_redirect( $url );
+					wp_safe_redirect( esc_url_raw( $url ) );
 					exit;
 
 				default:
 					/**
-					 * Backwards compatibility action
+					 * Allow extensions to handle their own CTA types.
+					 *
+					 * @param int $popup_id The popup ID if any.
+					 * @param CTA $cta The CTA object.
+					 *
+					 * @since X.X.X
 					 */
 					do_action( 'pum_cta_' . $cta->get_setting( 'type' ) . '_action', $popup_id, $cta );
 
-					/**
-					 * Default to current URL without CTA parameters
-					 */
-					wp_safe_redirect( remove_query_arg( [ 'pum_action', 'popup_id', 'cta_id' ] ) );
+					// Default to current URL without CTA parameters.
+					$url = remove_query_arg( $cta_args );
+
+					wp_safe_redirect( esc_url_raw( $url ) );
 					exit;
 			}
 		}
