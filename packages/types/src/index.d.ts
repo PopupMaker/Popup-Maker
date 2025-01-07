0725316701eecfe80fd045229f0e7de84d710abd
Daniel Iser <daniel@code-atlantic.com>
2025-01-06 21:06:32 -0500

    Enhance global type definitions with labels

--- packages/types/src/index.d.ts
+++ packages/types/src/index.d.ts
@@ -2,27 +2,82 @@
  * Global types for Popup Maker
  */
 
+/**
+ * The permissions for the current user.
+ */
 export interface PopupMakerPermissions {
+	/**
+	 * Whether the user can edit call to actions.
+	 */
 	edit_ctas: boolean;
+	/**
+	 * Whether the user can edit popups.
+	 */
 	edit_popups: boolean;
+	/**
+	 * Whether the user can edit popup themes.
+	 */
 	edit_popup_themes: boolean;
+	/**
+	 * Whether the user can manage settings.
+	 */
 	mange_settings: boolean;
 }
 
+/**
+ * The global variables for the Popup Maker plugin.
+ */
 export interface PopupMakerGlobalVars {
+	/**
+	 * The version of the Popup Maker plugin.
+	 */
 	version: string;
+	/**
+	 * The version of the WordPress installation.
+	 */
 	wpVersion: number;
+	/**
+	 * The URL to the assets directory.
+	 */
 	assetsUrl: string;
+	/**
+	 * The URL to the admin directory.
+	 */
 	adminUrl: string;
+	/**
+	 * The URL to the plugin directory.
+	 */
 	pluginUrl: string;
+	/**
+	 * The nonce for the current request.
+	 */
 	nonce: string;
+	/**
+	 * The permissions for the current user.
+	 */
 	permissions: PopupMakerPermissions;
+	/**
+	 * Whether the Pro version is installed.
+	 */
 	isProInstalled?: '1' | '';
+	/**
+	 * Whether the Pro version is activated.
+	 */
 	isProActivated?: '1' | '';
 }
+
+/**
+ * The window object for the Popup Maker plugin.
+ *
+ * window.popupMaker
+ */
 export interface PopupMakerWindow {
+	/**
+	 * The global variables for the Popup Maker plugin.
+	 */
 	globalVars: PopupMakerGlobalVars;
 }
+
 export interface WordPressWindow {
 	oldEditor: {
 		initialize: ( id: string, settings: any ) => void;
@@ -30,6 +85,7 @@ export interface WordPressWindow {
 	};
 	blocks?: unknown;
 }
+
 declare global {
 	interface Window {
 		wp: WordPressWindow & Record< string, unknown >;
