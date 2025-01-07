f3e46979c1cb078b9d560b258592a7b52be86312
Daniel Iser <daniel@code-atlantic.com>
2025-01-07 01:19:30 -0500

    Fix paths

--- packages/cta-editor/src/utils/index.ts
+++ packages/cta-editor/src/utils/index.ts
@@ -4,7 +4,7 @@ import { PopupMakerGlobalVars } from '@popup-maker/types';
  * Get the Popup Maker global variables
  */
 export const getGlobalVars = (): Partial< PopupMakerGlobalVars > => {
-	if ( typeof window === 'undefined' || ! window.popupMaker.globals ) {
+	if ( typeof window === 'undefined' || ! window.popupMaker.globalVars ) {
 		// Provide fallback values when running in block editor or SSR
 		return {
 			version: '1.0.0',
@@ -21,7 +21,7 @@ export const getGlobalVars = (): Partial< PopupMakerGlobalVars > => {
 			},
 		};
 	}
-	return window.popupMaker.globals;
+	return window.popupMaker.globalVars;
 };
 
 /**
