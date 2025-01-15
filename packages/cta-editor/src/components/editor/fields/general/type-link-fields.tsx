ccfaf975b81090978bae084b4839e9e954d33d3e
Daniel Iser <daniel@code-atlantic.com>
2025-01-15 18:48:06 -0500

    Remove commented code

--- packages/cta-editor/src/components/editor/fields/general/type-link-fields.tsx
+++ packages/cta-editor/src/components/editor/fields/general/type-link-fields.tsx
@@ -1,6 +1,5 @@
 import { __ } from '@wordpress/i18n';
 import { addFilter } from '@wordpress/hooks';
-import { useInstanceId } from '@wordpress/compose';
 
 import { FieldPanel, FieldRow, URLControl } from '@popup-maker/components';
 
@@ -13,8 +12,6 @@ const LinkFields = ( {
 	settings: CallToAction[ 'settings' ];
 	updateSettings: ( settings: Partial< CallToAction[ 'settings' ] > ) => void;
 } ) => {
-	const instanceId = useInstanceId( LinkFields );
-
 	return (
 		<FieldPanel title={ __( 'URL', 'popup-maker' ) }>
 			<FieldRow label={ __( 'Target URL', 'popup-maker' ) }>
