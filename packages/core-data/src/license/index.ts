d99e3eaaa9b6db9f651029f543f1d10d243119b8
Daniel Iser <daniel@code-atlantic.com>
2025-01-23 03:32:24 -0500

    Remove eroneous comments

--- packages/core-data/src/license/index.ts
+++ packages/core-data/src/license/index.ts
@@ -2,16 +2,9 @@ export * from './constants';
 export * from './store';
 export * from './types';
 
-// Export custom named variables.
 export { default as store } from './store';
 
 export {
-	/**
-	 * The default values for the License store.
-	 */
 	defaultValues as defaultLicenseValues,
-	/**
-	 * The name of the License store.
-	 */
 	STORE_NAME as LICENSE_STORE,
 } from './constants';
--- packages/core-data/src/settings/index.ts
+++ packages/core-data/src/settings/index.ts
@@ -2,16 +2,9 @@ export * from './constants';
 export * from './store';
 export * from './types';
 
-// Export custom named variables.
 export { default as store } from './store';
 
 export {
-	/**
-	 * The default values for the Settings store.
-	 */
 	defaultValues as defaultSettings,
-	/**
-	 * The name of the Settings store.
-	 */
 	STORE_NAME as SETTINGS_STORE,
 } from './constants';
