7a6be827bb3f8340e283a381711c68b10f6070d0
Daniel Iser <daniel@code-atlantic.com>
2025-01-30 22:53:56 -0500

    Export curried selectors as that is how we intend to use them

--- packages/core-data/src/call-to-actions/index.ts
+++ packages/core-data/src/call-to-actions/index.ts
@@ -1,3 +1,8 @@
+import type {
+	CurriedSelectorsOf,
+	DispatchReturn,
+} from '@wordpress/data/src/types';
+
 export { default as callToActionStore } from './store';
 export * from './validation';
 
@@ -7,9 +12,15 @@ export {
 } from './constants';
 
 export type * from './types/posttype';
+
+import type { StoreDescriptor, StoreState } from './types/store';
+
+type Selectors = CurriedSelectorsOf< StoreDescriptor >;
+type Actions = DispatchReturn< StoreDescriptor >;
+
 export type {
 	StoreDescriptor as CallToActionStore,
 	StoreState as CallToActionStoreState,
-	StoreActions as CallToActionStoreActions,
-	StoreSelectors as CallToActionStoreSelectors,
-} from './types/store';
+	Selectors as CallToActionStoreSelectors,
+	Actions as CallToActionStoreActions,
+};
