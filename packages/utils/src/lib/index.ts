ea02ca6277577f259ceb1f1425c639810ada7b94
Daniel Iser <daniel@code-atlantic.com>
2025-01-31 03:17:21 -0500

    Publish utils package

--- packages/utils/src/lib/index.ts
+++ packages/utils/src/lib/index.ts
@@ -2,4 +2,3 @@ export { default as clamp } from './clamp';
 export { default as noop } from './noop';
 export { default as omit } from './omit';
 export { default as pick } from './pick';
-export { default as saveFile } from './save-file';
--- packages/utils/src/lib/save-file.ts
+++
@@ -1,9 +0,0 @@
-const saveFile = ( content: string, fileName: string, contentType: string ) => {
-	const a = document.createElement( 'a' );
-	const file = new Blob( [ content ], { type: contentType } );
-	a.href = URL.createObjectURL( file );
-	a.download = fileName;
-	a.click();
-};
-
-export default saveFile;
