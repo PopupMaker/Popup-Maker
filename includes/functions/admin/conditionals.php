bd244fcc5e3ea2ef91f29c8c208dbce9170fee7a
Daniel L. Iser <daniel@code-atlantic.com>
2025-05-29 03:56:59 -0400

    Simplified functions, removed unneccessary cache

--- includes/functions/admin/conditionals.php
+++ includes/functions/admin/conditionals.php
@@ -14,29 +14,25 @@ if ( ! defined( 'ABSPATH' ) ) {
  *  Determines whether the current page is an popup maker admin page.
  *
  * @since 1.7.0
- * @since X.X.X Simplified & added cache.
  *
  * @return bool
  */
 function pum_is_admin_page() {
-	static $check;
-
-	if ( ! isset( $check ) ) {
-		$check = is_admin();
-
-		if ( ! $check ) {
-			return false;
-		}
-
-		$typenow = pum_typenow();
-
-		$check = 'popup' === $typenow
-			|| 'popup_theme' === $typenow
-			|| (
-				! empty( $GLOBALS['hook_suffix'] ) && in_array( $GLOBALS['hook_suffix'], PUM_Admin_Pages::$pages, true ) );
+	if ( ! is_admin() ) {
+		return false;
 	}
 
-	return $check;
+	$typenow = pum_typenow();
+
+	if ( 'popup' === $typenow || 'popup_theme' === $typenow ) {
+		return true;
+	}
+
+	if ( ! empty( $GLOBALS['hook_suffix'] ) && in_array( $GLOBALS['hook_suffix'], PUM_Admin_Pages::$pages, true ) ) {
+		return true;
+	}
+
+	return false;
 }
 
 
@@ -44,67 +40,44 @@ function pum_is_admin_page() {
  * Determines whether the current admin page is the All Popups page.
  *
  * @since 1.12
- * @since X.X.X Simplified & added cache.
  *
  * @return bool True if current page is All Popups page.
  */
 function pum_is_all_popups_page() {
-	static $check;
-
-	if ( ! isset( $check ) ) {
-		$screen = get_current_screen();
-		$check  = pum_is_admin_page()
-			&& 'edit-popup' === $screen->id
-			&& pum_typenow() === 'popup';
-	}
-
-	return $check;
+	$screen = get_current_screen();
+	return pum_is_admin_page()
+		&& 'edit-popup' === $screen->id
+		&& pum_typenow() === 'popup';
 }
 
 /**
  * Determines whether the current admin page is the popup editor.
  *
  * @since 1.7.0
- * @since X.X.X Simplified & added cache.
  *
  * @return bool
  */
 function pum_is_popup_editor() {
-	static $check;
+	global $pagenow;
 
-	if ( ! isset( $check ) ) {
-		global $pagenow;
-
-		$check = is_admin()
-			&& pum_is_admin_page()
-			&& 'popup' === pum_typenow()
-			&& in_array( $pagenow, [ 'post-new.php', 'post.php' ], true );
-	}
-
-	return $check;
+	return  pum_is_admin_page()
+		&& 'popup' === pum_typenow()
+		&& in_array( $pagenow, [ 'post-new.php', 'post.php' ], true );
 }
 
 /**
  * Determines whether the current admin page is the popup theme editor.
  *
  * @since 1.7.0
- * @since X.X.X Simplified & added cache.
  *
  * @return bool
  */
 function pum_is_popup_theme_editor() {
-	static $check;
+	global $pagenow;
 
-	if ( ! isset( $check ) ) {
-		global $pagenow;
-
-		$check = is_admin()
-			&& pum_is_admin_page()
-			&& 'popup_theme' === pum_typenow()
-			&& in_array( $pagenow, [ 'post-new.php', 'post.php' ], true );
-	}
-
-	return $check;
+	return pum_is_admin_page()
+		&& 'popup_theme' === pum_typenow()
+		&& in_array( $pagenow, [ 'post-new.php', 'post.php' ], true );
 }
 
 /**
