342c535060b50b8dab611ac0480564a264eacf39
Daniel L. Iser <daniel@code-atlantic.com>
2025-05-29 03:56:31 -0400

    Remove old CTA attempt

--- packages/block-library/src/index.ts
+++ packages/block-library/src/index.ts
@@ -1,12 +1,3 @@
-/**
- * WordPress dependencies
- */
 import { registerBlockType } from '@wordpress/blocks';
-/**
- * Internal dependencies
- */
-import * as cta from './calltoaction';
 
-[ cta ].forEach( ( { name, settings } ) =>
-	registerBlockType( name, settings )
-);
+[].forEach( ( { name, settings } ) => registerBlockType( name, settings ) );
