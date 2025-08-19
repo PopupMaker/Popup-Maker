a3adc349a265fd5054dcbb7760fac5e8cd2b7afe
Daniel L. Iser <daniel@code-atlantic.com>
2025-08-19 16:50:15 -0400

    Improve settings page tab navigation and hash handling
    - Extract tab switching logic into reusable function
    - Add hashchange listener for dynamic navigation
    - Clear hash on manual tab clicks to fix Go Pro link behavior
    - Fixes issue where Go Pro link doesn't work when already on #go-pro tab

--- assets/js/src/admin/settings-page/index.js
+++ assets/js/src/admin/settings-page/index.js
@@ -13,15 +13,8 @@ import './pro-upgrade-flow';
 
 	// Initiate when ready.
 	$( function () {
-		var $container = $( '#pum-settings-container' ),
-			args = pum_settings_editor.form_args || {},
-			values = pum_settings_editor.current_values || {};
-
-		if ( $container.length ) {
-			$container.find( '.pum-no-js' ).hide();
-			PUM_Admin.forms.render( args, values, $container );
-
-			// Universal tab deep linking - check hash and activate corresponding tab
+		// Universal tab deep linking - check hash and activate corresponding tab
+		function switchToHashTab() {
 			const hash = window.location.hash.replace( '#', '' );
 			if ( hash ) {
 				// Try direct tab ID first (e.g. #pum-settings_licenses)
@@ -35,6 +28,39 @@ import './pro-upgrade-flow';
 					setTimeout( () => $tab.trigger( 'click' ), 100 );
 				}
 			}
+		}
+
+		var $container = $( '#pum-settings-container' ),
+			args = pum_settings_editor.form_args || {},
+			values = pum_settings_editor.current_values || {};
+
+		if ( $container.length ) {
+			$container.find( '.pum-no-js' ).hide();
+			PUM_Admin.forms.render( args, values, $container );
+
+			// Check hash on page load
+			switchToHashTab();
+
+			// Listen for hash changes (Go Pro link clicks while on settings page)
+			$( window ).on( 'hashchange', switchToHashTab );
+
+			// Clear hash when any tab is clicked to allow Go Pro link to work when already on #go-pro
+			$container.on(
+				'click',
+				'.pum-tabs-container a[href^="#"]',
+				function () {
+					setTimeout( () => {
+						if ( window.location.hash ) {
+							history.replaceState(
+								null,
+								null,
+								window.location.pathname +
+									window.location.search
+							);
+						}
+					}, 50 );
+				}
+			);
 		}
 	} );
 } )( jQuery );
