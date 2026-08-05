62b5f4d1c242ea8244b2a971bc0cdbf4aaeca5d5
Daniel L. Iser <daniel@code-atlantic.com>
2026-08-05 03:58:52 -0400

    Fix WPBakery popup editor compatibility (#1270)
    * Fix WPBakery popup editor compatibility
    
    * Add WPBakery compatibility changelog entry
    
    * Use Mockery in WPBakery compatibility tests
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- classes/Controllers/Compatibility.php
+++ classes/Controllers/Compatibility.php
@@ -31,6 +31,7 @@ class Compatibility extends Controller {
 			'Compatibility\Builder\Astra'          => new \PopupMaker\Controllers\Compatibility\Builder\Astra( $this->container ),
 			'Compatibility\SEO\Yoast'              => new \PopupMaker\Controllers\Compatibility\SEO\Yoast( $this->container ),
 			'Compatibility\Builder\Divi'           => new \PopupMaker\Controllers\Compatibility\Builder\Divi( $this->container ),
+			'Compatibility\Builder\WPBakery'       => new \PopupMaker\Controllers\Compatibility\Builder\WPBakery( $this->container ),
 			'Compatibility\Plugin\ACF'             => new \PopupMaker\Controllers\Compatibility\Plugin\ACF( $this->container ),
 			'Compatibility\Plugin\Jetpack'         => new \PopupMaker\Controllers\Compatibility\Plugin\Jetpack( $this->container ),
 			'Compatibility\Plugin\PostDuplication' => new \PopupMaker\Controllers\Compatibility\Plugin\PostDuplication( $this->container ),
