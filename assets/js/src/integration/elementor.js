73e188d7b4ef5aacc1cc0d48c55901dc1d7172e1
Daniel L. Iser <daniel@code-atlantic.com>
2026-08-08 06:52:29 -0400

    Remove redundant Elementor widget refresh

--- assets/js/src/integration/elementor.js
+++ assets/js/src/integration/elementor.js
@@ -4,49 +4,25 @@
 {
 	const formProvider = 'elementor';
 	const $ = window.jQuery;
-	const refreshedElements = new WeakSet();
-
-	const refreshWidgets = ( popup ) => {
-		if (
-			window.elementorFrontend &&
-			window.elementorFrontend.elementsHandler &&
-			typeof window.elementorFrontend.elementsHandler.runReadyTrigger ===
-				'function'
-		) {
-			$( popup )
-				.find( '.elementor [data-element_type]' )
-				.each( function () {
-					if ( refreshedElements.has( this ) ) {
-						return;
-					}
-
-					window.elementorFrontend.elementsHandler.runReadyTrigger(
-						this
-					);
-					refreshedElements.add( this );
-				} );
-		}
-	};
 
 	// Elementor Forms success event.
-	$( document ).on( 'submit_success', '.elementor-form', function () {
-		const $form = $( this );
+	$( document ).on(
+		'submit_success',
+		'.elementor-form',
+		function ( event, response ) {
+			const $form = $( this )[ 0 ];
 
-		// Get element_id from the widget container.
-		// Elementor form widgets are inside a .elementor-element-{id} container.
-		const $widget = $( this ).closest( '[data-id]' );
-		const elementId = $widget.length
-			? $widget.attr( 'data-id' )
-			: 'unknown';
+			// Get element_id from the widget container.
+			// Elementor form widgets are inside a .elementor-element-{id} container.
+			const $widget = $( this ).closest( '[data-id]' );
+			const elementId = $widget.length
+				? $widget.attr( 'data-id' )
+				: 'unknown';
 
-		window.PUM.integrations.formSubmission( $form, {
-			formProvider,
-			formId: elementId,
-		} );
-	} );
-
-	// Reinitialize Elementor widgets after their popup becomes visible.
-	$( document ).on( 'pumAfterOpen.pumElementor', '.pum', function () {
-		refreshWidgets( this );
-	} );
+			window.PUM.integrations.formSubmission( $form, {
+				formProvider,
+				formId: elementId,
+			} );
+		}
+	);
 }
